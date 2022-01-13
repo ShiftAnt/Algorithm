86b5b8bdf699f2533d6dfec7cc3b7c23676f2f40
김지웅 <kjw4611@gmail.com>
2022-01-13 12:14:37 +0900

    [Coroutines] suspend function

--- src/main/kotlin/coroutine/Coroutine.kt
+++ src/main/kotlin/coroutine/Coroutine.kt
@@ -5,9 +5,11 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    launch {
-        delay(1000L)
-        println("World!")
-    }
+    launch { doWorld() }
     println("Hello")
+}
+
+suspend fun doWorld() {
+    delay(1000L)
+    println("World!")
 }
