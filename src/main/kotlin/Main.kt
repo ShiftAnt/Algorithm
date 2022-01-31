04b4c56cdb8bda66f328e722f0329db93d06771e
김지웅 <kjw4611@gmail.com>
2022-01-31 14:32:17 +0900

    [BJ 2234] 성곽

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,115 +1,76 @@
 import java.io.*
-import kotlin.math.*
+import kotlin.math.max
 
 var n = 0
 var m = 0
-var f = 0
-val que = ArrayDeque<IntArray>()
-var vstd = arrayOf<BooleanArray>()
-val fnd = ArrayList<IntArray>()
-var cur = intArrayOf()
-var Q = arrayOf<IntArray>()
-var P = arrayOf<IntArray>()
-fun init() {
-	que.clear()
-	vstd = Array(n) { BooleanArray(n) }
-	fnd.clear()
-}
-val dr = arrayOf(-1, 0, 0, 1)
-val dc = arrayOf(0, -1, 1, 0)
-fun bfs(target: Int): Int {
-	que.add(cur)
-	vstd[cur[0]][cur[1]] = true
+lateinit var P: Array<IntArray>
+lateinit var vstd: Array<IntArray>
 
-	var step = 0
-	while (!que.isEmpty()) {
-		val siz = que.size
-		for (tc in 0 until siz) {
-			val cur = que.removeFirst()
+val dr = arrayOf(0, -1, 0, 1)
+val dc = arrayOf(-1, 0, 1, 0)
 
-			if (target < 0) {
-				if (P[cur[0]][cur[1]] < 0) {
-					fnd.add(cur)
-				}
-			} else {
-				if (cur[0] == Q[target][2] && cur[1] == Q[target][3]) return step
-			}
+fun bfs(y: Int, x: Int, idx: Int): Int {
+	val que = ArrayDeque<Pair<Int, Int>>()
+	que.add(y to x)
+	vstd[y][x] = idx
+	var ret = 1
+	while (que.isNotEmpty()) {
+		val cur = que.removeFirst()
 
-			for (i in 0 until 4) {
-				val y = cur[0] + dr[i]
-				val x = cur[1] + dc[i]
+		for (i in 0 until 4) {
+			val a = cur.first + dr[i]
+			val b = cur.second + dc[i]
 
-				if (y in 0 until n && x in 0 until n && !vstd[y][x] && P[y][x] != 1) {
-					vstd[y][x] = true
-					que.add(intArrayOf(y, x))
-				}
+			if (P[cur.first][cur.second].and(1.shl(i)) == 0 && vstd[a][b] == 0) {
+				vstd[a][b] = idx
+				que.add(a to b)
+				++ret
 			}
 		}
-		if (fnd.isNotEmpty()) {
-			val mi = intArrayOf(n, n)
-
-			for (ele in fnd) {
-				if (mi[0] > ele[0]) {
-					mi[0] = ele[0]
-					mi[1] = ele[1]
-				} else if (mi[0] == ele[0]) {
-					mi[1] = min(mi[1], ele[1])
-				}
-			}
-			val mie = -P[mi[0]][mi[1]] - 1
-			if (step <= f) {
-				f -= step
-				init()
-				cur[0] = Q[mie][0]
-				cur[1] = Q[mie][1]
-				P[cur[0]][cur[1]] = 0
-			} else return -1
-			val nxt = bfs(mie)
-			if (nxt == -1) return -1
-			return if (nxt <= f) {
-				f += nxt
-				init()
-				cur[0] = Q[mie][2]
-				cur[1] = Q[mie][3]
-				--m
-				f
-			} else -1
-
-		}
-		++step
 	}
-	return -1
+	return ret
 }
-
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-	br.readLine().split(" ").map { it.toInt() }.let {
-		n = it[0]
-		m = it[1]
-		f = it[2]
+	br.readLine().split(" ").map { it.toInt() }.run {
+		n = this[1]
+		m = this[0]
 	}
+
 	P = Array(n) { intArrayOf() }
 	repeat(n) {
 		P[it] = br.readLine().split(" ").map { it.toInt() }.toIntArray()
 	}
-	cur = br.readLine().split(" ").map { it.toInt() - 1 }.toIntArray()
-	Q = Array(m) { intArrayOf() }
-	repeat(m) {
-		Q[it] = br.readLine().split(" ").map { it.toInt() - 1 }.toIntArray()
-		P[Q[it][0]][Q[it][1]] = -it - 1
-	}
-	init()
+	vstd = Array(n) { IntArray(m) }
+	var idx = 0
 
-	while (m != 0) {
-		if (bfs(-1) == -1) {
-			break
+	val al = ArrayList<Int>()
+	for (i in 0 until n) {
+		for (j in 0 until m) {
+			if (vstd[i][j] == 0) {
+				al.add(bfs(i, j, ++idx))
+			}
 		}
 	}
-	if (m != 0) bw.write("-1\n")
-	else bw.write("$f\n")
+	val a = al.size
+	val b = al.maxOf { it }
+	var c = b
 
+	for (i in 0 until n) {
+		for (j in 0 until m) {
+			val cur = vstd[i][j]
+			for (k in 2 until 4) {
+				val y = i + dr[k]
+				val x = j + dc[k]
+				if (y in 0 until n && x in 0 until m && cur != vstd[y][x]) {
+					c = max(c, al[cur - 1] + al[vstd[y][x] - 1])
+				}
+			}
+		}
+	}
+	bw.write("$a\n$b\n$c\n")
 	bw.close()
 	br.close()
 }
