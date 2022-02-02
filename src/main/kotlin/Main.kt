5c650c4c0487bd4e3daf63310cbc76c7fa7b28b5
김지웅 <kjw4611@gmail.com>
2022-02-02 16:02:58 +0900

    [BJ 10653] 마라톤 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,40 @@
 import java.io.*
+import kotlin.math.*
 
+fun dis(a: IntArray, b: IntArray) = abs(a[0] - b[0]) + abs(a[1] - b[1])
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val (n, m, h) = br.readLine().split(" ").map { it.toInt() }
+    val (n, k) = br.readLine().split(" ").map { it.toInt() }
 
-    val dp = IntArray(h + 1)
-    dp[0] = 1
+    val P = Array(n) {IntArray(2)}
 
-    repeat(n) {
-        val cur = br.readLine().split(" ").map { it.toInt() }
+    val dp = Array(n) {IntArray(k + 1)}
 
-        for (i in h downTo 0) {
-            if (dp[i] != 0) {
-                for (j in cur.indices) {
-                    if (i + cur[j] <= h) {
-                        dp[cur[j] + i] += dp[i]
-                        dp[cur[j] + i] %= 10007
-                    }
-                }
-            }
+    for (i in 0 until n) {
+        for (j in 0 until k + 1) {
+            dp[i][j] = Int.MAX_VALUE
         }
     }
 
-    bw.write("${dp[h]}\n")
+    repeat(n) {
+        P[it] = br.readLine().split(" ").map { it.toInt() }.toIntArray()
+    }
+    dp[0][0] = 0
+    dp[1][0] = dis(P[0], P[1])
+    for (i in 2 until n) {
+        for (j in 0 until i) {
+            val cur = dis(P[i], P[j])
+            for (y in 0 until k + 1) {
+                if (dp[j][y] == Int.MAX_VALUE) break
+                if (i - j - 1 + y <= k) {
+                    dp[i][i - j - 1 + y] = min(dp[i][i - j - 1 + y], dp[j][y] + cur)
+                } else break
+            }
+        }
+    }
+    bw.write("${dp[n - 1].minOf { it }}")
 
     bw.close()
     br.close()
