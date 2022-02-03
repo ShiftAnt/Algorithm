0c55d13d990f2f7d6fc3646bb47655b11c10e951
김지웅 <kjw4611@gmail.com>
2022-02-03 14:25:36 +0900

    [BJ 8972] 미친 아두이노

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,103 @@
 import java.io.*
 import kotlin.math.*
 
-fun dis(a: IntArray, b: IntArray) = abs(a[0] - b[0]) + abs(a[1] - b[1])
+var n = 0
+var m = 0
+var P = arrayOf<StringBuilder>()
+var cmds = ""
+var cur = 0 to 0
+var crz = ArrayDeque<Pair<Int, Int>>()
+
+val dr = arrayOf(1, 1, 1, 0, 0, 0, -1, -1, -1)
+val dc = arrayOf(-1, 0, 1, -1, 0, 1, -1, 0, 1)
+
+fun dis (a: Pair<Int, Int>, b: Pair<Int, Int>) = abs(a.first - b.first) + abs(a.second - b.second)
+fun move(tar: Pair<Int, Int>): Pair<Int, Int> {
+	var mi = Int.MAX_VALUE
+	var idx = -1
+	for (i in dr.indices) {
+		val y = tar.first + dr[i]
+		val x = tar.second + dc[i]
+		val d = dis(cur, y to x)
+		if (mi > d) {
+			mi = d
+			idx = i
+		}
+	}
+	return tar.first + dr[idx] to tar.second + dc[idx]
+}
+
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
+	val br = BufferedReader(InputStreamReader(System.`in`))
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+	br.readLine().split(" ").map { it.toInt() }.let {
+		n = it[0]
+		m = it[1]
+	}
+	P = Array(n) { StringBuilder() }
 
-    val P = Array(n) {IntArray(2)}
+	repeat(n) {
+		P[it] = StringBuilder(br.readLine())
+		for (i in 0 until m) {
+			if (P[it][i] == 'I') cur = it to i
+			else if (P[it][i] == 'R') crz += it to i
+		}
+	}
 
-    val dp = Array(n) {IntArray(k + 1)}
+	cmds = br.readLine()
+	var ret = -1
+	loop@
+	for (tc in cmds.indices) {
+		val cmd = cmds[tc] - '0' - 1
+		P[cur.first][cur.second] = '.'
+		cur = cur.first + dr[cmd] to cur.second + dc[cmd]
+	   if (P[cur.first][cur.second] == 'R') {
+		   ret = tc + 1
+		   break
+	   } else P[cur.first][cur.second] = 'I'
 
-    for (i in 0 until n) {
-        for (j in 0 until k + 1) {
-            dp[i][j] = Int.MAX_VALUE
-        }
-    }
+		for (i in crz.indices) {
+			P[crz[i].first][crz[i].second] = '.'
+			crz[i] = move(crz[i])
+		}
+		val del = ArrayList<Pair<Int, Int>>()
+		for (i in crz.indices) {
+			val y = crz[i].first
+			val x = crz[i].second
+			when (P[y][x]) {
+				'I' -> {
+					ret = tc + 1
+					break@loop
+				}
+				'.' -> {
+					P[y][x] = 'R'
+				}
+				'R' -> {
+					P[y][x] = 'X'
+					del.add(y to x)
+				}
+			}
+		}
+		val siz = crz.size
+		for (i in 0 until siz) {
+			val it = crz.removeFirst()
+			if (P[it.first][it.second] != 'X') {
+				crz += it
+			}
+		}
+		for (i in del.indices) {
+			P[del[i].first][del[i].second] = '.'
+		}
+	}
 
-    repeat(n) {
-        P[it] = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    }
-    dp[0][0] = 0
-    dp[1][0] = dis(P[0], P[1])
-    for (i in 2 until n) {
-        for (j in 0 until i) {
-            val cur = dis(P[i], P[j])
-            for (y in 0 until k + 1) {
-                if (dp[j][y] == Int.MAX_VALUE) break
-                if (i - j - 1 + y <= k) {
-                    dp[i][i - j - 1 + y] = min(dp[i][i - j - 1 + y], dp[j][y] + cur)
-                } else break
-            }
-        }
-    }
-    bw.write("${dp[n - 1].minOf { it }}")
+	if (ret == -1) {
+		repeat(n) {
+			bw.write("${P[it]}\n")
+		}
+	}
+	else bw.write("kraj $ret\n")
 
-    bw.close()
-    br.close()
+	bw.close()
+	br.close()
 }
