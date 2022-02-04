2dc940d5bb7910dd2b2c741be94cd1c97d07a0f1
김지웅 <kjw4611@gmail.com>
2022-02-04 11:08:29 +0900

    [BJ 14466] 소가 길을 건너간 이유 6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,102 +1,70 @@
 import java.io.*
-import kotlin.math.*
 
-var n = 0
-var m = 0
-var P = arrayOf<StringBuilder>()
-var cmds = ""
-var cur = 0 to 0
-var crz = ArrayDeque<Pair<Int, Int>>()
-
-val dr = arrayOf(1, 1, 1, 0, 0, 0, -1, -1, -1)
-val dc = arrayOf(-1, 0, 1, -1, 0, 1, -1, 0, 1)
-
-fun dis (a: Pair<Int, Int>, b: Pair<Int, Int>) = abs(a.first - b.first) + abs(a.second - b.second)
-fun move(tar: Pair<Int, Int>): Pair<Int, Int> {
-	var mi = Int.MAX_VALUE
-	var idx = -1
-	for (i in dr.indices) {
-		val y = tar.first + dr[i]
-		val x = tar.second + dc[i]
-		val d = dis(cur, y to x)
-		if (mi > d) {
-			mi = d
-			idx = i
-		}
+fun calDif(a: Int, b: Int) = when (a) {
+	-1 -> 0
+	1 -> 3
+	else -> when (b) {
+		-1 -> 1
+		1 -> 2
+		else -> 4
 	}
-	return tar.first + dr[idx] to tar.second + dc[idx]
 }
-
+val dr = arrayOf(-1, 0, 0, 1)
+val dc = arrayOf(0, -1, 1, 0)
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-	br.readLine().split(" ").map { it.toInt() }.let {
-		n = it[0]
-		m = it[1]
+	val (N, K, R) = br.readLine().split(" ").map { it.toInt() }
+
+	val P = Array(N) { IntArray(N) }
+
+	repeat(R) {
+		val a = br.readLine().split(" ").map { it.toInt() - 1 }
+		P[a[0]][a[1]] += 1.shl(calDif(a[2] - a[0], a[3] - a[1]))
+		P[a[2]][a[3]] += 1.shl(calDif(a[0] - a[2], a[1] - a[3]))
 	}
-	P = Array(n) { StringBuilder() }
+	repeat(K) {
+		val a = br.readLine().split(" ").map { it.toInt() - 1 }
 
-	repeat(n) {
-		P[it] = StringBuilder(br.readLine())
-		for (i in 0 until m) {
-			if (P[it][i] == 'I') cur = it to i
-			else if (P[it][i] == 'R') crz += it to i
-		}
+		P[a[0]][a[1]] += 1.shl(4)
 	}
 
-	cmds = br.readLine()
-	var ret = -1
-	loop@
-	for (tc in cmds.indices) {
-		val cmd = cmds[tc] - '0' - 1
-		P[cur.first][cur.second] = '.'
-		cur = cur.first + dr[cmd] to cur.second + dc[cmd]
-	   if (P[cur.first][cur.second] == 'R') {
-		   ret = tc + 1
-		   break
-	   } else P[cur.first][cur.second] = 'I'
+	val vstd = Array(N) {BooleanArray(N)}
+	val al = ArrayList<Int>()
+	for (i in 0 until N) {
+		for (j in 0 until N) {
+			if (!vstd[i][j]) {
+				val que = ArrayDeque<Pair<Int, Int>>()
+				que += i to j
+				var cnt = 0
+				vstd[i][j] = true
+				while (que.isNotEmpty()) {
+					val cur = que.removeFirst()
+					if (P[cur.first][cur.second].and(1.shl(4)) != 0) ++cnt
+					for (k in 0 until 4) {
+						val y = cur.first + dr[k]
+						val x = cur.second + dc[k]
 
-		for (i in crz.indices) {
-			P[crz[i].first][crz[i].second] = '.'
-			crz[i] = move(crz[i])
-		}
-		val del = ArrayList<Pair<Int, Int>>()
-		for (i in crz.indices) {
-			val y = crz[i].first
-			val x = crz[i].second
-			when (P[y][x]) {
-				'I' -> {
-					ret = tc + 1
-					break@loop
+						if (y in 0 until N && x in 0 until N && !vstd[y][x] && P[cur.first][cur.second].and(1.shl(k)) == 0) {
+							vstd[y][x] = true
+							que.add(y to x)
+						}
+					}
 				}
-				'.' -> {
-					P[y][x] = 'R'
-				}
-				'R' -> {
-					P[y][x] = 'X'
-					del.add(y to x)
-				}
+				al.add(cnt)
 			}
 		}
-		val siz = crz.size
-		for (i in 0 until siz) {
-			val it = crz.removeFirst()
-			if (P[it.first][it.second] != 'X') {
-				crz += it
-			}
-		}
-		for (i in del.indices) {
-			P[del[i].first][del[i].second] = '.'
-		}
+	}
+	var ret = 0
+	var tmp = K
+	for (a in al) {
+		tmp -= a
+		ret += tmp * a
 	}
 
-	if (ret == -1) {
-		repeat(n) {
-			bw.write("${P[it]}\n")
-		}
-	}
-	else bw.write("kraj $ret\n")
+	bw.write("$ret\n")
+
 
 	bw.close()
 	br.close()
