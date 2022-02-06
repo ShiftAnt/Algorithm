944948f55e6ce3b7af25361d9ddf9d1fb7e3ada5
김지웅 <kjw4611@gmail.com>
2022-02-06 10:55:31 +0900

    [BJ 10836] 여왕벌

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,70 +1,44 @@
 import java.io.*
-
-var n = 0
-var q = 0
-val al = Array(6) {ArrayList<String>()}
-
-fun last(cur: ArrayList<String>, a: String): Int {
-	var s = 0
-	var e = cur.size - 1
-
-	while (s <= e) {
-		val mid = (s + e) / 2
-		if (cur[mid] <= a) {
-			if (mid == cur.size - 1 || cur[mid + 1] > a) return mid
-			s = mid + 1
-		} else e = mid - 1
-	}
-	return -1
-}
-
-fun least(cur: ArrayList<String>, a: String): Int {
-	var s = 0
-	var e = cur.size - 1
-
-	while (s <= e) {
-		val mid = (s + e) / 2
-		if (cur[mid] >= a) {
-			if (mid == 0 || cur[mid - 1] < a) return mid
-			e = mid - 1
-		} else {
-			s = mid + 1
-		}
-	}
-	return -1
-}
-
-fun fnd(stt: String, end: String, lv: Int): Int {
-	var ret = 0
-	for (t in lv - 1 until 6) {
-		val si = least(al[t], stt)
-		ret += if (si == -1) 0 else last(al[t], end) - si + 1
-	}
-	return ret
-}
+import kotlin.math.max
 
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-	br.readLine().split(" ").map { it.toInt() }.let {
-		n = it[0]
-		q = it[1]
+	val (m, n) = br.readLine().split(" ").map { it.toInt() }
+
+	val P = IntArray(2 * m - 1)
+	val ret = Array(m) {IntArray(m)}
+	repeat(m) {
+		ret[it].fill(1)
 	}
-	repeat(n) {
-		br.readLine().split("#").let {
-			al[it[1].toInt() - 1] += it[0]
+	for (i in 0 until n) {
+		var idx = 0
+		val inp = br.readLine().split(" ").map { it.toInt() }
+		for (j in 0 until 3) {
+			for (k in 0 until inp[j]) {
+				P[idx++] += j
+			}
 		}
 	}
-	repeat(6) {
-		al[it].sort()
+	for (i in 0 until m) {
+		ret[m - 1 - i][0] += P[i]
+	}
+	for (i in m until 2 * m - 1) {
+		ret[0][i - m + 1] += P[i]
+	}
+	for (i in 1 until m) {
+		for (j in 1 until m) {
+			ret[i][j] = max(max(ret[i - 1][j - 1], ret[i][j - 1]), ret[i - 1][j])
+		}
+	}
+	for (i in 0 until m) {
+		for (j in 0 until m) {
+			bw.write("${ret[i][j]} ")
+		}
+		bw.write("\n")
 	}
 
-	repeat(q) {
-		br.readLine().split("#").let {
-			bw.write("${fnd(it[0], it[1], it[2].toInt())}\n")
-		}
-	}
 	bw.close()
 	br.close()
 }
