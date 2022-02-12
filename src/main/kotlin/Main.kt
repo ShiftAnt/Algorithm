291bd7f28fd5bc815c3289b6098c197de656909a
김지웅 <kjw4611@gmail.com>
2022-02-12 22:13:02 +0900

    [BJ 18428] 감시 피하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,71 @@
 import java.io.*
 
+
+var n = 0
+var arr = arrayOf(0, 1, 2)
+var P = arrayOf<CharArray>()
+val dr = arrayOf(-1, 0, 0, 1)
+val dc = arrayOf(0, -1, 1, 0)
+fun isBlank(idx: Int): Boolean {
+	return P[idx / n][idx % n] == 'X'
+}
+
+fun fill(idx: Int, c: Char) {
+	P[idx / n][idx % n] = c
+}
+
+fun check(): Boolean {
+	for (t in ts) {
+		val y = t.first
+		val x = t.second
+		for (i in 0 until 4) {
+			var a = y + dr[i]
+			var b = x + dc[i]
+
+			while (a in 0 until n && b in 0 until n && P[a][b] != 'O') {
+				if (P[a][b] == 'S') return false
+				a += dr[i]
+				b += dc[i]
+			}
+		}
+	}
+	return true
+}
+
+val ts = ArrayList<Pair<Int, Int>>()
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-	val (n, w, L) = br.readLine().split(" ").map { it.toInt() }
+	n = br.readLine().toInt()
 
-	val ts = br.readLine().split(" ").map { it.toInt() }
+	P = Array(n) { charArrayOf() }
 
-	val que = ArrayDeque<Int>()
-
-	var time = 0
-	var sum = 0
-	var idx = 0
-	while (true) {
-		if (idx == n) {
-			time += w
-			break
-		}
-		++time
-		if (que.size < w) {
-			if (sum + ts[idx] <= L) {
-				que += ts[idx]
-				sum += ts[idx++]
-			} else que += 0
-		} else {
-			sum -= que.removeFirst()
-			if (sum + ts[idx] <= L) {
-				que += ts[idx]
-				sum += ts[idx++]
-
-			} else que += 0
+	repeat(n) {
+		P[it] = br.readLine().split(" ").map { it[0] }.toCharArray()
+		for (i in 0 until n) {
+			if (P[it][i] == 'T') ts.add(it to i)
 		}
 	}
-
-	bw.write("$time")
+	var ret = "NO"
+	while (arr[0] != n * n - 2) {
+		if (isBlank(arr[0]) && isBlank(arr[1]) && isBlank(arr[2])) {
+			for (i in arr.indices) fill(arr[i], 'O')
+			if (check()) {
+				ret = "YES"
+				break
+			}
+			for (i in arr.indices) fill(arr[i], 'X')
+		}
+		if (++arr[2] == n * n) {
+			if (++arr[1] == n * n - 1) {
+				++arr[0]
+				arr[1] = arr[0] + 1
+			}
+			arr[2] = arr[1] + 1
+		}
+	}
+	bw.write(ret)
 	bw.close()
 	br.close()
 }
