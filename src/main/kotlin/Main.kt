c7eaed8f47bb27cb33c0445ff2f0336e08cc3e62
김지웅 <kjw4611@gmail.com>
2022-02-13 11:35:20 +0900

    [BJ 11967] 불켜기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,71 +1,61 @@
 import java.io.*
 
-
 var n = 0
-var arr = arrayOf(0, 1, 2)
-var P = arrayOf<CharArray>()
+var m = 0
+var P = arrayOf<IntArray>()
+var al = arrayOf<Array<ArrayList<Pair<Int, Int>>>>()
 val dr = arrayOf(-1, 0, 0, 1)
 val dc = arrayOf(0, -1, 1, 0)
-fun isBlank(idx: Int): Boolean {
-	return P[idx / n][idx % n] == 'X'
-}
-
-fun fill(idx: Int, c: Char) {
-	P[idx / n][idx % n] = c
-}
-
-fun check(): Boolean {
-	for (t in ts) {
-		val y = t.first
-		val x = t.second
-		for (i in 0 until 4) {
-			var a = y + dr[i]
-			var b = x + dc[i]
-
-			while (a in 0 until n && b in 0 until n && P[a][b] != 'O') {
-				if (P[a][b] == 'S') return false
-				a += dr[i]
-				b += dc[i]
-			}
-		}
-	}
-	return true
-}
-
-val ts = ArrayList<Pair<Int, Int>>()
+var ret = 1
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-	n = br.readLine().toInt()
-
-	P = Array(n) { charArrayOf() }
-
-	repeat(n) {
-		P[it] = br.readLine().split(" ").map { it[0] }.toCharArray()
-		for (i in 0 until n) {
-			if (P[it][i] == 'T') ts.add(it to i)
+	br.readLine().split(" ").map { it.toInt() }.let {
+		n = it[0]
+		m = it[1]
+	}
+	P = Array(n) { IntArray(n) }
+	al = Array(n) {Array(n) { ArrayList() } }
+	P[0][0] = 2
+	repeat(m) {
+		br.readLine().split(" ").map { it.toInt() - 1 }.let {
+			al[it[0]][it[1]] += it[2] to it[3]
 		}
 	}
-	var ret = "NO"
-	while (arr[0] != n * n - 2) {
-		if (isBlank(arr[0]) && isBlank(arr[1]) && isBlank(arr[2])) {
-			for (i in arr.indices) fill(arr[i], 'O')
-			if (check()) {
-				ret = "YES"
-				break
+	val que = ArrayDeque<Pair<Int, Int>>()
+	que += 0 to 0
+
+	while (!que.isEmpty()) {
+		val cur = que.removeFirst()
+
+		for (light in al[cur.first][cur.second]) {
+			if (P[light.first][light.second] == 0) {
+				++ret
+				P[light.first][light.second] = 1
+				for (i in dr.indices) {
+					val y = light.first + dr[i]
+					val x = light.second + dc[i]
+					if (y in 0 until n && x in 0 until n && P[y][x] == 2) {
+						que.add(light)
+						P[light.first][light.second] = 2
+						break
+					}
+				}
 			}
-			for (i in arr.indices) fill(arr[i], 'X')
 		}
-		if (++arr[2] == n * n) {
-			if (++arr[1] == n * n - 1) {
-				++arr[0]
-				arr[1] = arr[0] + 1
+
+		for (i in dr.indices) {
+			val y = cur.first + dr[i]
+			val x = cur.second + dc[i]
+			if (y in 0 until n && x in 0 until n && P[y][x] == 1) {
+				P[y][x] = 2
+				que.add(y to x)
 			}
-			arr[2] = arr[1] + 1
 		}
 	}
-	bw.write(ret)
+
+	bw.write("$ret")
 	bw.close()
 	br.close()
 }
