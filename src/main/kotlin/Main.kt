796a5d9e4be797bef0f8a4b5c104c341abf309cc
김지웅 <kjw4611@gmail.com>
2022-02-16 09:57:30 +0900

    [BJ 2310] 어드벤처 게임

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,50 @@
-import java.util.*
-import kotlin.math.*
+import java.io.*
 
 fun main() {
-	val sc = Scanner(System.`in`)
-	val n = sc.nextInt()
-	val P = IntArray(n)
+	val br = BufferedReader(InputStreamReader(System.`in`))
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-	repeat(n) {
-		P[it] = sc.nextInt()
-	}
-
-	val dp = Array(n) {IntArray(2)}
-	dp[0][0] = 1
-
-	for (i in 1 until n) {
-		dp[i][0] = 1
-		for (j in i - 1 downTo 0) {
-			if (P[i] < P[j]) dp[i][0] = max(dp[i][0], dp[j][0] + 1)
+	while (true) {
+		val n = br.readLine().toInt()
+		if (n == 0) break
+		val P = ArrayList<Pair<String, Int>>()
+		val nxts = Array(n) {ArrayList<Int>()}
+		repeat(n) { tc ->
+			br.readLine().split(" ").let {
+				P += it[0] to it[1].toInt()
+				for (i in 2 until it.size - 1) {
+					nxts[tc] += it[i].toInt() - 1
+				}
+			}
 		}
-	}
-
-	for (i in n - 2 downTo 0) {
-		for (j in i + 1 until n) {
-			if (P[i] < P[j]) dp[i][1] = max(dp[i][1], dp[j][1] + 1)
+		val vstd = IntArray(n)
+		vstd.fill(-1)
+		var ret = "No"
+		val que = ArrayDeque<Pair<Int, Int>>()
+		que += 0 to 0
+		vstd[0] = 0
+		while (!que.isEmpty()) {
+			val cur = que.removeFirst()
+			val money = when (P[cur.first].first) {
+				"L" -> if (cur.second < P[cur.first].second) P[cur.first].second else cur.second
+				"T" -> cur.second - P[cur.first].second
+				else -> cur.second
+			}
+			if (money < 0) continue
+			if (cur.first == n - 1) {
+				ret = "Yes"
+				break
+			}
+			for (nxt in nxts[cur.first]) {
+				if (vstd[nxt] < money) {
+					vstd[nxt] = money
+					que += nxt to money
+				}
+			}
 		}
+		bw.write("$ret\n")
 	}
-	var ret = 0
 
-	for (i in 0 until n) {
-		ret = max(ret, dp[i][0] + dp[i][1])
-	}
-	print("$ret\n")
+	bw.close()
+	br.close()
 }
