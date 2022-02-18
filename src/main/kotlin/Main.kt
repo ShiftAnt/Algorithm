42eff5fd9df4703ac2982591558dd60276e536a5
김지웅 <kjw4611@gmail.com>
2022-02-18 12:31:51 +0900

    [BJ 11812] K진 트리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,33 @@
 import java.io.*
 import kotlin.math.*
 
-const val INF = Long.MAX_VALUE / 2
+var n = 0L
+var k = 0
+var q = 0
+
+fun parent(num: Long) = (num + k - 2) / k
+
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
-
-	val (n, m) = br.readLine().split(" ").map { it.toInt() }
-
-	val al = Array(m) { intArrayOf() }
-
-	repeat(m) {
-		al[it] = br.readLine().split(" ").mapIndexed {index, s ->
-			if (index < 2) s.toInt() - 1 else s.toInt()
-		}.toIntArray()
+	br.readLine().split(" ").map { it.toLong() }.let {
+		n = it[0]
+		k = it[1].toInt()
+		q = it[2].toInt()
 	}
 
-	val P = LongArray(n)
-	P.fill(INF)
-	P[0] = 0
-
-	for (i in 0 until n) {
-		for (cur in al) {
-			if (P[cur[0]] != INF) {
-				P[cur[1]] = min(P[cur[1]], P[cur[0]] + cur[2])
-			}
+	for (i in 0 until q) {
+		var (a, b) = br.readLine().split(" ").map { it.toLong() }
+		var ret = 0
+		if (k == 1) {
+			bw.write("${abs(a - b)}\n")
+			continue
 		}
-	}
-	var isINF = false
-	for (cur in al) {
-		if (P[cur[0]] != INF && P[cur[1]] > P[cur[0]] + cur[2]) {
-			isINF = true
-			break
+		while (a != b) {
+			if (a > b) a = parent(a) else b = parent(b)
+			++ret
 		}
+		bw.write("$ret\n")
 	}
-	if (isINF) {
-		bw.write("-1\n")
-	} else {
-		for (i in 1 until n) {
-			bw.write("${if (P[i] == INF) -1 else P[i]}\n")
-		}
-	}
-
 	bw.close()
-	br.close()
 }
