809f03ff4724b7e06ffa9c92d2bbc54668d8cb2a
김지웅 <kjw4611@gmail.com>
2022-02-22 23:26:22 +0900

    [BJ 14442] 벽 부수고 이동하기 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,56 @@
 import java.io.*
-import kotlin.math.*
 
-const val MX = 1000001
+val dr = arrayOf(-1, 0, 0, 1)
+val dc = arrayOf(0, -1, 1, 0)
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-	val (n, k) = br.readLine().split(" ").map { it.toInt() }
+	val (n, m, k) = br.readLine().split(" ").map { it.toInt() }
 
-	val vstd = Array(MX) {BooleanArray(11)}
-	val que = ArrayDeque<IntArray>()
+	val P = Array(n) {StringBuilder()}
 
+	repeat(n) {
+		P[it] = StringBuilder(br.readLine())
+	}
+
+	val que = ArrayDeque<Triple<Int, Int, Int>>()
+
+	val vstd = Array(n) {Array(m) {BooleanArray(k + 1)} }
+
+	vstd[0][0][k] = true
+
+	que.add(Triple(0, 0, k))
+	var cnt = 0
 	var ret = -1
-
-	que.add(intArrayOf(n, k))
-	vstd[n][k] = true
+	loop@
 	while (!que.isEmpty()) {
-		val a = que.first()[0]
-		val b = que.removeFirst()[1]
-		if (b == 0) {
-			ret = max(ret, a)
-			continue
-		}
-		val cur = StringBuilder(a.toString())
-		for (i in cur.indices) {
-			for (j in i + 1 until cur.length) {
-				if (i == 0 && cur[j] == '0') continue
-				cur[i] = cur[j].also { cur[j] = cur[i] }
-				val nxt = cur.toString().toInt()
-				if (!vstd[nxt][b - 1]) {
-					que.add(intArrayOf(nxt, b - 1))
-					vstd[nxt][b - 1] = true
+		++cnt
+		for (tc in que.indices) {
+			val cur = que.removeFirst()
+			val z = cur.third
+			if (cur.first == n - 1 && cur.second == m - 1) {
+				ret = cnt
+				break@loop
+			}
+			for (i in 0 until 4) {
+				val y = cur.first + dr[i]
+				val x = cur.second + dc[i]
+
+				if (y in 0 until n && x in 0 until m) {
+					if (P[y][x] == '0' && !vstd[y][x][z]) {
+						vstd[y][x][z] = true
+						que.add(Triple(y, x, z))
+					}
+					if (P[y][x] == '1' && z > 0 && !vstd[y][x][z - 1]) {
+						vstd[y][x][z - 1] = true
+						que.add(Triple(y, x, z - 1))
+					}
 				}
-				cur[i] = cur[j].also { cur[j] = cur[i] }
 			}
 		}
 	}
-	bw.write("$ret\n")
+	bw.write("$ret")
 	bw.close()
 	br.close()
 }
