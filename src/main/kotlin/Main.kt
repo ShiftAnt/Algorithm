4340e06adc69001647f085fa52fec0227990b746
김지웅 <kjw4611@gmail.com>
2022-02-26 10:37:26 +0900

    [BJ 16933] 벽 부수고 이동하기 3(다른 풀이)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 import java.io.*
+import java.util.PriorityQueue
 
 class Node(
 	val y: Int,
 	val x: Int,
 	val k: Int,
-	val isNight: Int
+	val isNight: Int,
+    val ret: Int
 )
 val dr = arrayOf(-1, 0, 0, 1)
 val dc = arrayOf(0, -1, 1, 0)
@@ -16,49 +18,44 @@ fun main() {
 
 	val P = Array(n) {br.readLine()}
 
-	val vstd = Array(n) {Array(m) {Array(k + 1) {BooleanArray(2)} } }
+	val vstd = Array(n) {IntArray(m)}
+    repeat(n) {
+        vstd[it].fill(-1)
+    }
 
-	val que = ArrayDeque<Node>()
+	val que = PriorityQueue<Node>(compareBy { it.ret })
 
-	vstd[0][0][k][0] = true
+	vstd[0][0] = k
 
-	que.add(Node(0, 0, k, 0))
-	var step = 0
+	que.add(Node(0, 0, k, 0, 1))
+	que.add(Node(0, 0, k, 1, 2))
 	var ret = -1
+    if (n == 1 && m == 1) ret = 1
 	loop@
 	while (!que.isEmpty()) {
-		++step
-		for (tc in que.indices) {
-			val cur = que.removeFirst()
-			if (cur.y == n - 1 && cur.x == m - 1) {
-				ret = step
-				break@loop
-			}
-			for (i in dr.indices) {
-				val y = cur.y + dr[i]
-				val x = cur.x + dc[i]
+        val cur = que.poll()
+        for (i in dr.indices) {
+            val y = cur.y + dr[i]
+            val x = cur.x + dc[i]
+            if (y in 0 until n && x in 0 until m) {
+                if (y == n - 1 && x == m - 1) {
+                    ret = cur.ret + 1
+                    break@loop
+                }
+                if (P[y][x] == '0' && vstd[y][x] < cur.k) {
+                    vstd[y][x] = cur.k
+                    que.add(Node(y, x, cur.k, 1 - cur.isNight, cur.ret + 1))
+                    que.add(Node(y, x, cur.k, cur.isNight, cur.ret + 2))
+                }
+                else if (cur.isNight == 0 && P[y][x] == '1' && cur.k > 0 && vstd[y][x] < cur.k - 1) {
+                    vstd[y][x] = cur.k - 1
+                    que.add(Node(y, x, cur.k - 1, 1, cur.ret + 1))
+                    que.add(Node(y, x, cur.k - 1, 0, cur.ret + 2))
+                }
+            }
+        }
 
-				if (y in 0 until n && x in 0 until m) {
-                    if (y == n - 1 && x == m - 1) {
-                        ret = step + 1
-                        break@loop
-                    }
-					if (P[y][x] == '0' && !vstd[y][x][cur.k][1 - cur.isNight]) {
-						vstd[y][x][cur.k][1 - cur.isNight] = true
-						que.add(Node(y, x, cur.k, 1 - cur.isNight))
-					}
-					else if (cur.isNight == 0 && P[y][x] == '1' && cur.k > 0 && !vstd[y][x][cur.k - 1][1]) {
-						vstd[y][x][cur.k - 1][1] = true
-						que.add(Node(y, x, cur.k - 1, 1))
-					}
-				}
-			}
-			if (!vstd[cur.y][cur.x][cur.k][1 - cur.isNight]) {
-				vstd[cur.y][cur.x][cur.k][1 - cur.isNight] = true
-				que.add(Node(cur.y, cur.x, cur.k, 1 - cur.isNight))
-			}
-		}
 	}
-	bw.write("$ret")
+    bw.write("$ret")
 	bw.flush()
 }
