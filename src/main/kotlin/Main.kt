87e8086bae1146cf474e203d642eb99e5782f2a0
김지웅 <kjw4611@gmail.com>
2022-02-28 15:52:47 +0900

    [BJ 16954] 움직이는 미로 탈출

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,48 @@
 import java.io.*
-val dr = arrayOf(0, 1, 1, 1)
-val dc = arrayOf(1, -1, 0, 1)
+val dr = arrayOf(-1, -1, -1, 0, 0, 0, 1, 1, 1)
+val dc = arrayOf(-1, 0, 1, -1, 0, 1, -1, 0, 1)
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
-	val bw = BufferedWriter(OutputStreamWriter(System.out))
-	val n = 3
-	while (true) {
-		val cur = br.readLine()
-		if (cur == "end") break
-		val P = Array(n) {CharArray(n)}
+	val n = 8
+	val P = Array(n + 1) {Array(n) {""} }
 
-		repeat(9) {
-			P[it / n][it % n] = cur[it]
-		}
-		var o = 0
-		var x = 0
-		for (i in 0 until n) {
-			for (j in 0 until n) {
-				if (P[i][j] == 'O') ++o
-				else if (P[i][j] == 'X') ++x
-			}
-		}
-		if (x - o != 0 && x - o != 1) {
-			bw.write("invalid\n")
-			continue
-		}
-		var co = false
-		var cx = false
-		for (i in 0 until n) {
-			for (j in 0 until n) {
-				if (P[i][j] == '.') continue
-				for (k in dr.indices) {
-					val y = i + dr[k] * (n - 1)
-					val x = j + dc[k] * (n - 1)
-					if (y in 0 until n && x in 0 until n && P[i][j] == P[y][x] && P[i][j] == P[i + dr[k]][j + dc[k]]) {
-                        if (P[i][j] == 'O') co = true
-                        else cx = true
-					}
-				}
-			}
-		}
-		var ret = "valid"
-		if (co && cx || cx && x == o || co && o != x || !co && !cx && o + x != n * n) ret = "invalid"
-		bw.write("$ret\n")
+
+	repeat(n) {
+		P[0][it] = br.readLine()
 	}
-	bw.flush()
+
+	for (idx in 1..n) {
+			P[idx][0] = "........"
+		for (i in 1 until n) {
+			P[idx][i] = P[idx - 1][i - 1]
+		}
+	}
+	val vstd = Array(n + 1) {Array(n) {BooleanArray(n)} }
+
+	val que = ArrayDeque<Triple<Int, Int, Int>>()
+	que.add(Triple(0, n - 1, 0))
+
+	vstd[0][n - 1][0] = true
+	var ret = 0
+	loop@
+	while (!que.isEmpty()) {
+		val cur = que.removeFirst()
+
+		val nxt = (cur.first + 1).let { if (it > 8) 8 else it }
+		for (i in dr.indices) {
+			val y = cur.second + dr[i]
+			val x = cur.third + dc[i]
+
+			if (y in 0 until n && x in 0 until n && !vstd[nxt][y][x] && P[cur.first][y][x] == '.') {
+				if (y == 0 && x == n - 1) {
+					ret = 1
+					break@loop
+				}
+				if (P[nxt][y][x] == '#') continue
+				vstd[nxt][y][x] = true
+				que.add(Triple(nxt, y, x))
+			}
+		}
+	}
+	println(ret)
 }
