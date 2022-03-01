26506ef30edd5c02ab531ced148c81d6e1cc7bd4
김지웅 <kjw4611@gmail.com>
2022-03-01 12:05:36 +0900

    [BJ 13913] 숨바꼭질 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,48 @@
 import java.io.*
-val dr = arrayOf(-1, -1, -1, 0, 0, 0, 1, 1, 1)
-val dc = arrayOf(-1, 0, 1, -1, 0, 1, -1, 0, 1)
-fun main() {
-	val br = BufferedReader(InputStreamReader(System.`in`))
-	val n = 8
-	val P = Array(n + 1) {Array(n) {""} }
 
-
-	repeat(n) {
-		P[0][it] = br.readLine()
+fun solution(): List<Int> {
+	val (n, k) = readLine()!!.split(" ").map { it.toInt()}
+	val ret = ArrayList<Int>()
+	if (n == k) {
+		ret.add(n)
+		return ret
 	}
+	val mx = 200001
+	val P = IntArray(mx)
+	P.fill(-1)
+	val que = ArrayDeque<Int>()
+	que.add(n)
 
-	for (idx in 1..n) {
-			P[idx][0] = "........"
-		for (i in 1 until n) {
-			P[idx][i] = P[idx - 1][i - 1]
-		}
-	}
-	val vstd = Array(n + 1) {Array(n) {BooleanArray(n)} }
-
-	val que = ArrayDeque<Triple<Int, Int, Int>>()
-	que.add(Triple(0, n - 1, 0))
-
-	vstd[0][n - 1][0] = true
-	var ret = 0
+	P[n] = n
 	loop@
 	while (!que.isEmpty()) {
 		val cur = que.removeFirst()
+		val nxts = arrayOf(cur - 1, cur + 1 , cur * 2)
 
-		val nxt = (cur.first + 1).let { if (it > 8) 8 else it }
-		for (i in dr.indices) {
-			val y = cur.second + dr[i]
-			val x = cur.third + dc[i]
-
-			if (y in 0 until n && x in 0 until n && !vstd[nxt][y][x] && P[cur.first][y][x] == '.') {
-				if (y == 0 && x == n - 1) {
-					ret = 1
-					break@loop
-				}
-				if (P[nxt][y][x] == '#') continue
-				vstd[nxt][y][x] = true
-				que.add(Triple(nxt, y, x))
+		for (nxt in nxts) {
+			if (nxt in 0 until mx && P[nxt] == -1) {
+				P[nxt] = cur
+				if (nxt == k) break@loop
+				que.add(nxt)
 			}
 		}
 	}
-	println(ret)
+	var cur = k
+	while (cur != n) {
+		ret.add(cur)
+		cur = P[cur]
+	}
+	ret.add(cur)
+	return ret.reversed()
+}
+
+fun main() {
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
+	solution().let {
+		bw.write("${it.size - 1}\n")
+		it.forEach { ele ->
+			bw.write("$ele ")
+		}
+	}
+	bw.flush()
 }
