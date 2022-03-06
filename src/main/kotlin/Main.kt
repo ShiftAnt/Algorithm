5ee71030bd946d378e269c118aa09cb0991a1cc5
김지웅 <kjw4611@gmail.com>
2022-03-06 12:08:23 +0900

    [BJ 2482] 색상환 (fix)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,19 @@
-import java.io.*
 import java.util.Scanner
-import kotlin.math.*
-
+const val MOD = 1000000003
 fun main() {
 	val sc = Scanner(System.`in`)
-	val bw = BufferedWriter(OutputStreamWriter(System.out))
 	val n = sc.nextInt()
-	val P = IntArray(n)
-	repeat(n) {
-		P[it] = sc.nextInt()
+	val k = sc.nextInt()
+	if (n / 2 < k) {
+		println(0)
+		return
 	}
-	val al = ArrayList<Pair<Int, Int>>()
+	val dp = Array(n + 1) {IntArray(k + 1) }
 
-	for (tc in 0 until sc.nextInt()) {
-		var a = sc.nextInt()
-		var b = sc.nextInt()
-		if (a <= b) a = b
-		var cur = a to b
-		while (al.isNotEmpty()) {
-			val lst = al.last()
-			if (max(cur.first, cur.second) >= max(lst.first, lst.second)) al.removeLast()
-			else if (cur.first >= lst.second || cur.second >= lst.second) {
-				cur = lst.first to cur.second
-				al.removeLast()
-			} else break
+	for (i in 2..n) {
+		for (j in 1..k) {
+			dp[i][j] = if (j == 1) i else (dp[i - 2][j - 1] + dp[i - 1][j]) % MOD
 		}
-		al.add(cur)
 	}
-	val que = ArrayDeque<Int>()
-	repeat(al[0].first) {
-		que.add(P[it])
-	}
-	que.sort()
-	val ret = ArrayDeque<Int>()
-	var cur = al[0].first
-	for (nxt in al) {
-		for (i in nxt.first until cur) ret.addFirst(que.removeFirst())
-		cur = nxt.first
-		for (i in nxt.second until cur) ret.addFirst(que.removeLast())
-		cur = nxt.second
-	}
-	for (i in 0 until cur) ret.addFirst(que.removeFirst())
-
-	for (i in al[0].first until n) {
-		ret.addLast(P[i])
-	}
-	repeat(n) {
-		bw.write("${ret[it]} ")
-	}
-	bw.flush()
+	println(dp[n][k])
 }
