ac96fd417c3f34bfa1aabf910e3bd3168ca704e0
김지웅 <kjw4611@gmail.com>
2022-03-08 09:36:45 +0900

    [BJ 1717] 집합의 표현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,40 @@
 import java.io.*
-import kotlin.math.*
+var n = 0
+var m = 0
+var P = arrayOf<IntArray>()
 
+fun djs(a: Int, set: Int = -1): Int {
+	var cur = a
+
+	while (cur != P[cur][0]) {
+		cur = P[cur][0].also { if (set != -1) P[cur][0] = set}
+	}
+	if (set != -1) P[cur][0] = set
+	return cur
+}
+
+fun merge(a: Int, b: Int) {
+	val da = djs(a)
+	val db = djs(b)
+	val set = if (P[da][1] < P[db][1]) db else da
+	djs(a, set)
+	djs(b, set)
+}
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
-	val n = br.readLine().toInt()
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
 	br.readLine().split(" ").map { it.toInt() }.let {
-		var a = 0
-		var b = n - 1
-		var ret = 0
-		while (a < b) {
-			ret = max((b - a - 1) * min(it[a], it[b]), ret)
-			if (it[a] <= it[b]) ++a
-			else --b
-		}
-		println(ret)
+		n = it[0]; m = it[1]
 	}
+	P = Array(n + 1) { IntArray(2) }
+	repeat(n + 1) {
+		P[it][0] = it
+		P[it][1] = 1
+	}
+	repeat(m) {
+		val it = br.readLine().split(" ").map { it.toInt() }
+		if (it[0] == 0) merge(it[1], it[2])
+		else bw.write(if (djs(it[1]) == djs(it[2])) "YES\n" else "NO\n")
+	}
+	bw.flush()
 }
