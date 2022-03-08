5fbe7a9c7ce39e663fbf4eaf8c8a831111ed1035
김지웅 <kjw4611@gmail.com>
2022-03-09 07:08:35 +0900

    [BJ 9177] 단어 섞기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,24 @@
 import java.io.*
-var n = 0
-var m = 0
-var P = arrayOf<IntArray>()
+import kotlin.math.*
 
-fun djs(a: Int, set: Int = -1): Int {
-	var cur = a
-
-	while (cur != P[cur][0]) {
-		cur = P[cur][0].also { if (set != -1) P[cur][0] = set}
-	}
-	if (set != -1) P[cur][0] = set
-	return cur
-}
-
-fun merge(a: Int, b: Int) {
-	val da = djs(a)
-	val db = djs(b)
-	val set = if (P[da][1] < P[db][1]) db else da
-	djs(a, set)
-	djs(b, set)
-}
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
 	val bw = BufferedWriter(OutputStreamWriter(System.out))
-	br.readLine().split(" ").map { it.toInt() }.let {
-		n = it[0]; m = it[1]
-	}
-	P = Array(n + 1) { IntArray(2) }
-	repeat(n + 1) {
-		P[it][0] = it
-		P[it][1] = 1
-	}
-	repeat(m) {
-		val it = br.readLine().split(" ").map { it.toInt() }
-		if (it[0] == 0) merge(it[1], it[2])
-		else bw.write(if (djs(it[1]) == djs(it[2])) "YES\n" else "NO\n")
+
+	for (tc in 1..br.readLine().toInt()) {
+		val (A, B, C) = br.readLine().split(" ")
+		val a = A.length
+		val b = B.length
+		val c = C.length
+		val P = Array(a + 1) {IntArray(b + 1)}
+
+		for (i in 0..a) {
+			for (j in 0..b) {
+				if (i != 0 && A[i - 1] == C[i + j - 1]) P[i][j] = max(P[i][j], P[i - 1][j] + 1)
+				if (j != 0 && B[j - 1] == C[i + j - 1]) P[i][j] = max(P[i][j], P[i][j - 1] + 1)
+			}
+		}
+		bw.write("Data set $tc: ${if (P[a][b] == c) "yes" else "no"}\n")
 	}
 	bw.flush()
 }
