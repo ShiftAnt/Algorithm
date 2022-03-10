5d1a73cc66b1e4abbf7328370521c01ea63262d3
김지웅 <kjw4611@gmail.com>
2022-03-10 10:46:41 +0900

    [BJ 11758] CCW

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,4 @@
-import java.io.*
-import kotlin.math.*
-
 fun main() {
-	val br = BufferedReader(InputStreamReader(System.`in`))
-	val bw = BufferedWriter(OutputStreamWriter(System.out))
-
-	for (tc in 1..br.readLine().toInt()) {
-		val (A, B, C) = br.readLine().split(" ")
-		val a = A.length
-		val b = B.length
-		val c = C.length
-		val P = Array(a + 1) {IntArray(b + 1)}
-
-		for (i in 0..a) {
-			for (j in 0..b) {
-				if (i != 0 && A[i - 1] == C[i + j - 1]) P[i][j] = max(P[i][j], P[i - 1][j] + 1)
-				if (j != 0 && B[j - 1] == C[i + j - 1]) P[i][j] = max(P[i][j], P[i][j - 1] + 1)
-			}
-		}
-		bw.write("Data set $tc: ${if (P[a][b] == c) "yes" else "no"}\n")
-	}
-	bw.flush()
+	val P = Array(3) { readLine()!!.split(" ").map { it.toInt() }}
+	println(((P[1][0] - P[0][0]) * (P[2][1] - P[0][1])).compareTo(((P[1][1] - P[0][1]) * (P[2][0] - P[0][0]))))
 }
