aa828c9b53c7049c0b04f1a766822920d13d23d1
김지웅 <kjw4611@gmail.com>
2022-03-13 12:40:02 +0900

    [BJ 2533] 사회망 서비스(SNS)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,40 @@
+import java.io.*
+
+var n = 0
+var al = arrayOf<ArrayList<Int>>()
+var vstd = booleanArrayOf()
+
+var ret = 0
+var early = booleanArrayOf()
+fun dfs(idx: Int) {
+    vstd[idx] = true
+    var childIdea = false
+    for (nxt in al[idx]) {
+        if (!vstd[nxt]) {
+            dfs(nxt)
+            if (!early[nxt]) {
+                childIdea = true
+            }
+        }
+    }
+    if (childIdea) {
+        early[idx] = true
+        ++ret
+    }
+}
+
 fun main() {
-	IntArray(readLine()!!.toInt()).also {
-		readLine()!!.split(" ").mapIndexed { i, s -> s.toInt() to i }
-			.sortedWith(compareBy({ it.first }, { it.second }))
-			.forEachIndexed { i, ele -> it[ele.second] = i }
-	}.forEach { print("$it ") }
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    n = br.readLine().toInt()
+    al = Array(n) {ArrayList()}
+    repeat(n - 1) {
+        br.readLine().split(" ").map { it.toInt() - 1 }.let {
+            al[it[0]].add(it[1])
+            al[it[1]].add(it[0])
+        }
+    }
+    early = BooleanArray(n)
+    vstd = BooleanArray(n)
+    dfs(0)
+    println(ret)
 }
