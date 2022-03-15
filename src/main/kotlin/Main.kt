9da4bffc3ee544cd9cb359a5e656cd80b9084b3e
김지웅 <kjw4611@gmail.com>
2022-03-15 16:05:23 +0900

    [BJ 10159] 저울

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,27 @@
 import java.io.*
-import java.util.StringTokenizer
 
-val dr = arrayOf(-1, 0, 0, 1)
-val dc = arrayOf(0, -1, 1, 0)
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    var st = StringTokenizer(br.readLine())
-    val (h, w, n) = Array(3) {st.nextToken().toInt()}
+	val br = BufferedReader(InputStreamReader(System.`in`))
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val P = Array(h) {StringBuilder(br.readLine())}
-    var cur = 0 to 0
+	val n = br.readLine().toInt()
+	val P = Array(n) {IntArray(n)}
 
-    repeat(h) {
-        for (i in 0 until w) {
-            if (P[it][i] == 'S') {
-                cur = it to i
-                P[it][i] = '.'
-                return@repeat
-            }
-        }
-    }
-    fun bfs(y: Int, x: Int, fnd: Char): Triple<Int, Int, Int> {
-        val que = ArrayDeque<Pair<Int, Int>>()
-        val vstd = Array(h) {BooleanArray(w)}
-        vstd[y][x] = true
-        que.add(y to x)
-        var ret = 0
-        while (!que.isEmpty()) {
-            val siz = que.size
-            ++ret
-            for (tc in 0 until siz) {
-                val c = que.removeFirst()
-                for (i in dr.indices) {
-                    val a = c.first + dr[i]
-                    val b = c.second + dc[i]
-                    if (a in 0 until h && b in 0 until w && P[a][b] != 'X' && !vstd[a][b]) {
-                        vstd[a][b] = true
-                        if (P[a][b] == fnd) {
-                            P[a][b] = '.'
-                            return Triple(a, b, ret)
-                        }
-                        que.add(a to b)
-                    }
-                }
-            }
-        }
-        return Triple(0, 0, -1)
-    }
-    var ret = 0
-    for (tc in '1'..n.toString()[0]) {
-        bfs(cur.first, cur.second, tc).let {
-            cur = it.first to it.second; ret += it.third
-        }
-    }
-    println(ret)
+	repeat(br.readLine().toInt()) {
+		br.readLine().split(" ").map { it.toInt() - 1 }.let {
+			P[it[0]][it[1]] = 1
+			P[it[1]][it[0]] = -1
+		}
+	}
+	for (k in 0 until n) {
+		for (i in 0 until n) {
+			for (j in 0 until n) {
+				if (P[i][k] != 0 && P[i][k] == P[k][j]) P[i][j] = P[i][k]
+			}
+		}
+	}
+	repeat(n) {
+		bw.write("${P[it].count { it == 0 } - 1}\n")
+	}
+	bw.flush()
 }
