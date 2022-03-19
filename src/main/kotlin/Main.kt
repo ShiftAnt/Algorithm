804aacae09fa5738017b1864b594d290c9078c75
김지웅 <kjw4611@gmail.com>
2022-03-19 09:09:53 +0900

    [BJ 21610] 마법사 상어와 비바라기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,77 @@
-fun main() {
-	val MOD = 1000000000
-	val (n, k) = readLine()!!.split(" ").map { it.toInt() }
+import java.io.*
 
-	val P = Array(n + 1) {IntArray(k + 1)}
+var n = 0
+var m = 0
+var P = arrayOf<IntArray>()
+var clds = arrayListOf<Node>()
 
-	for (i in 0..n) {
-		P[i][1] = 1
+class Node(
+	var y: Int,
+	var x: Int,
+)
+val dr = arrayOf(0, -1, -1, -1, 0, 1, 1, 1)
+val dc = arrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
+fun makePos(y: Int): Int {
+	var ret = y
+	while (ret < 0) ret += n
+	return ret % n
+}
+fun move(d: Int, s: Int) {
+	for (i in clds.indices) {
+		clds[i].y += dr[d] * s
+		clds[i].x += dc[d] * s
+		clds[i].y = makePos(clds[i].y)
+		clds[i].x = makePos(clds[i].x)
 	}
-
-	for (j in 1 until k) {
-		for (i in 0..n) {
-			for (m in 0..n) {
-				if (i + m > n) break
-				P[i + m][j + 1] += P[i][j]
-				P[i + m][j + 1] %= MOD
+	for (cld in clds) {
+		++P[cld.y][cld.x]
+	}
+	for (cld in clds) {
+		for (i in 1 until dr.size step 2) {
+			val y = cld.y + dr[i]
+			val x = cld.x + dc[i]
+			if (y in 0 until n && x in 0 until n && P[y][x] > 0) {
+				++P[cld.y][cld.x]
 			}
 		}
 	}
-	println(P[n][k])
+	for (cld in clds) {
+		P[cld.y][cld.x] = -P[cld.y][cld.x]
+	}
+	val newClds = ArrayList<Node>()
+
+	for (i in 0 until n) {
+		for (j in 0 until n) {
+			if (P[i][j] >= 2) {
+				newClds.add(Node(i, j))
+				P[i][j] -= 2
+			}
+		}
+	}
+	for (cld in clds) {
+		P[cld.y][cld.x] = -P[cld.y][cld.x]
+	}
+	clds = newClds
+}
+
+fun main() {
+	val br = BufferedReader(InputStreamReader(System.`in`))
+
+	br.readLine().split(" ").map { it.toInt() }.let {
+		n = it[0]; m = it[1]
+	}
+	P = Array(n) {br.readLine().split(" ").map { it.toInt() }.toIntArray()}
+	clds = arrayListOf(Node(n - 1, 0), Node(n - 1, 1), Node(n - 2, 0), Node(n - 2, 1))
+	repeat(m) {
+		br.readLine().split(" ").map { it.toInt() }.let {
+			move(it[0] - 1, it[1])
+		}
+	}
+	var ret = 0
+	for (i in 0 until n) {
+		for (j in 0 until n) {
+			ret += P[i][j]
+		}
+	}
+	println(ret)
 }
