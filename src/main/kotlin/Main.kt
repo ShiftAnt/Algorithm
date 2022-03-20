8345bb860ecd5e13572480d397bffd1014bc66d2
김지웅 <kjw4611@gmail.com>
2022-03-20 09:36:36 +0900

    [BJ 1715] 카드 정렬하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,77 +1,17 @@
 import java.io.*
-
-var n = 0
-var m = 0
-var P = arrayOf<IntArray>()
-var clds = arrayListOf<Node>()
-
-class Node(
-	var y: Int,
-	var x: Int,
-)
-val dr = arrayOf(0, -1, -1, -1, 0, 1, 1, 1)
-val dc = arrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
-fun makePos(y: Int): Int {
-	var ret = y
-	while (ret < 0) ret += n
-	return ret % n
-}
-fun move(d: Int, s: Int) {
-	for (i in clds.indices) {
-		clds[i].y += dr[d] * s
-		clds[i].x += dc[d] * s
-		clds[i].y = makePos(clds[i].y)
-		clds[i].x = makePos(clds[i].x)
-	}
-	for (cld in clds) {
-		++P[cld.y][cld.x]
-	}
-	for (cld in clds) {
-		for (i in 1 until dr.size step 2) {
-			val y = cld.y + dr[i]
-			val x = cld.x + dc[i]
-			if (y in 0 until n && x in 0 until n && P[y][x] > 0) {
-				++P[cld.y][cld.x]
-			}
-		}
-	}
-	for (cld in clds) {
-		P[cld.y][cld.x] = -P[cld.y][cld.x]
-	}
-	val newClds = ArrayList<Node>()
-
-	for (i in 0 until n) {
-		for (j in 0 until n) {
-			if (P[i][j] >= 2) {
-				newClds.add(Node(i, j))
-				P[i][j] -= 2
-			}
-		}
-	}
-	for (cld in clds) {
-		P[cld.y][cld.x] = -P[cld.y][cld.x]
-	}
-	clds = newClds
-}
+import java.util.PriorityQueue
 
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
-
-	br.readLine().split(" ").map { it.toInt() }.let {
-		n = it[0]; m = it[1]
-	}
-	P = Array(n) {br.readLine().split(" ").map { it.toInt() }.toIntArray()}
-	clds = arrayListOf(Node(n - 1, 0), Node(n - 1, 1), Node(n - 2, 0), Node(n - 2, 1))
-	repeat(m) {
-		br.readLine().split(" ").map { it.toInt() }.let {
-			move(it[0] - 1, it[1])
-		}
+	val pq = PriorityQueue<Int>()
+	repeat(br.readLine().toInt()) {
+		pq.add(br.readLine().toInt())
 	}
 	var ret = 0
-	for (i in 0 until n) {
-		for (j in 0 until n) {
-			ret += P[i][j]
-		}
+	while (pq.size > 1) {
+		val sub = pq.poll() + pq.poll()
+		ret += sub
+		pq.add(sub)
 	}
 	println(ret)
 }
