976e0bc75c96ac9e4a8bb9deacb0d4485b179d1a
김지웅 <kjw4611@gmail.com>
2022-03-31 15:55:49 +0900

    [BJ 1949] 우수 마을

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,56 +1,46 @@
 import java.io.*
+import java.util.StringTokenizer
+import kotlin.math.*
+
 var n = 0
-var P = arrayListOf<IntArray>()
-var map = hashMapOf<String, Int>()
+var al = arrayOf<ArrayList<Int>>()
+var vstd = booleanArrayOf()
+var P = arrayOf<IntArray>()
 
-fun djs(a: Int, set: Int = -1): Int {
-	var cur = a
-	while (cur != P[cur][0]) {
-		val tmp = cur
-		cur = P[cur][0]
-		if (set != -1) P[tmp][0] = set
+fun dfs(idx: Int) {
+	vstd[idx] = true
+	var wbSum = 0
+	val nxts = ArrayList<Int>()
+	for (nxt in al[idx]) {
+		if (vstd[nxt]) continue
+		dfs(nxt)
+		nxts += nxt
+		wbSum += max(P[nxt][1], P[nxt][2])
 	}
-	if (set != -1) P[cur][0] = set
-	return cur
-}
-
-fun merge(a: Int, b: Int): Int {
-	val ta = djs(a)
-	val tb = djs(b)
-	if (ta == tb) return P[ta][1]
-	val f: Int
-	val t: Int
-	if (P[ta][1] < P[tb][1]) {
-		f = ta
-		t = tb
-	} else {
-		f = tb
-		t = ta
+	for (nxt in nxts) {
+		P[idx][0] += P[nxt][1]
+		P[idx][1] = max(P[idx][1], wbSum - max(P[nxt][1], P[nxt][2]) + P[nxt][2])
+		P[idx][2] += max(P[nxt][0], P[nxt][1])
 	}
-	P[t][1] += P[f][1]
-	djs(a, t)
-	djs(b, t)
-	return P[t][1]
 }
-
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
-	val bw = BufferedWriter(OutputStreamWriter(System.out))
-	repeat(br.readLine().toInt()) {
-		P = ArrayList()
-		map = HashMap()
-		var idx = 0
-		for (i in 0 until br.readLine().toInt()) {
-			br.readLine().split(" ").let {
-				for (j in 0 until 2) {
-					if (map[it[j]] == null) {
-						P += intArrayOf(idx, 1)
-						map[it[j]] = idx++
-					}
-				}
-				bw.write("${merge(map[it[0]]!!, map[it[1]]!!)}\n")
-			}
-		}
+
+	n = br.readLine().toInt()
+	vstd = BooleanArray(n)
+	P = Array(n) {IntArray(3)}
+	var st = StringTokenizer(br.readLine())
+	repeat(n) {
+		P[it][2] = st.nextToken().toInt()
 	}
-	bw.flush()
+	al = Array(n) {ArrayList()}
+	repeat(n - 1) {
+		st = StringTokenizer(br.readLine())
+		val a = st.nextToken().toInt() - 1
+		val b = st.nextToken().toInt() - 1
+		al[a] += b
+		al[b] += a
+	}
+	dfs(0)
+	println(max(P[0][1], P[0][2]))
 }
