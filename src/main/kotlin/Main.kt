c3bebd85e5eddef0e88bad4d3d7961ace1dca3f4
김지웅 <kjw4611@gmail.com>
2022-04-01 13:46:30 +0900

    [BJ 14725] 개미굴

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,28 @@
-import java.io.*
-import java.util.StringTokenizer
-import kotlin.math.*
+class Trie(
+	val name: String = "",
+	val nxt: HashMap<String, Trie> = HashMap()
+)
 
-var n = 0
-var al = arrayOf<ArrayList<Int>>()
-var vstd = booleanArrayOf()
-var P = arrayOf<IntArray>()
+fun hyphen(dept: Int) = StringBuilder().also { for (i in 0 until dept) it.append("--") }.toString()
 
-fun dfs(idx: Int) {
-	vstd[idx] = true
-	var wbSum = 0
-	val nxts = ArrayList<Int>()
-	for (nxt in al[idx]) {
-		if (vstd[nxt]) continue
-		dfs(nxt)
-		nxts += nxt
-		wbSum += max(P[nxt][1], P[nxt][2])
+fun dfs(cur: Trie, dept: Int) {
+	if (dept != -1) {
+		println("${hyphen(dept)}${cur.name}")
 	}
-	for (nxt in nxts) {
-		P[idx][0] += P[nxt][1]
-		P[idx][1] = max(P[idx][1], wbSum - max(P[nxt][1], P[nxt][2]) + P[nxt][2])
-		P[idx][2] += max(P[nxt][0], P[nxt][1])
+	cur.nxt.values.sortedBy { it.name }.forEach { nxt ->
+		dfs(nxt, dept + 1)
 	}
 }
-fun main() {
-	val br = BufferedReader(InputStreamReader(System.`in`))
 
-	n = br.readLine().toInt()
-	vstd = BooleanArray(n)
-	P = Array(n) {IntArray(3)}
-	var st = StringTokenizer(br.readLine())
-	repeat(n) {
-		P[it][2] = st.nextToken().toInt()
+fun main() {
+	val root = Trie()
+	repeat(readLine()!!.toInt()) {
+		val st = java.util.StringTokenizer(readLine())
+		var cur = root
+		repeat(st.nextToken().toInt()) {
+			val str = st.nextToken()
+			cur = cur.nxt[str] ?: run { cur.nxt[str] = Trie(str); cur.nxt[str]!! }
+		}
 	}
-	al = Array(n) {ArrayList()}
-	repeat(n - 1) {
-		st = StringTokenizer(br.readLine())
-		val a = st.nextToken().toInt() - 1
-		val b = st.nextToken().toInt() - 1
-		al[a] += b
-		al[b] += a
-	}
-	dfs(0)
-	println(max(P[0][1], P[0][2]))
+	dfs(root, -1)
 }
