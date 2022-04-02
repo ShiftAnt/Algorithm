fad9b1fa7178376893792b2eefb9f144194a2075
김지웅 <kjw4611@gmail.com>
2022-04-02 11:27:03 +0900

    [BJ 1684] 같은 나머지

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,17 @@
 import kotlin.math.*
 
+fun gcf(a: Int, b: Int): Int {
+	if (b == 0) return a
+	return gcf(b, a % b)
+}
 fun main() {
 	val n = readLine()!!.toInt()
-	val strs = Array(n) { readLine()!! }
-	val P = Array(10) { longArrayOf(0, 0) }
+	val st = java.util.StringTokenizer(readLine())
+	val P = IntArray(n) {st.nextToken().toInt()}
+	var ret = P[0] - P[1]
 
-	for (str in strs) {
-		for (i in str.indices) {
-			val alpha = str[i] - 'A'
-			if (i == 0) P[alpha][1] = 1
-			P[alpha][0] += 10.0.pow(str.length - 1 - i).toLong()
-		}
+	for (i in 1 until n - 1) {
+		ret = gcf(ret, P[i] - P[i + 1])
 	}
-	P.sortByDescending {it[0]}
-
-	var ret = 0L
-	var total = 0
-	var notFirst = -1
-
-	for (i in P.indices) {
-		if (P[i][0] != 0L) ++total
-		if (P[i][1] == 0L) notFirst = i
-	}
-	if (total == 10) {
-		for (i in notFirst until 9) {
-			P[i] = P[i + 1].also { P[i + 1] = P[i] }
-		}
-	}
-	for (i in 0 until total) {
-		ret += P[i][0] * (9 - i)
-	}
-	println(ret)
+	println(abs(ret))
 }
