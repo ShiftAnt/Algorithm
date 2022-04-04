9026f85d9e765dc4731545adab38197750732f4c
김지웅 <kjw4611@gmail.com>
2022-04-04 13:25:35 +0900

    [BJ 14906] 스러피

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,41 @@
 import java.io.*
-val dr = arrayOf(-1, 0, 0, 1)
-val dc = arrayOf(0, -1, 1, 0)
-fun main() {
-	val br = BufferedReader(InputStreamReader(System.`in`))
-	val (n, m) = br.readLine().split(" ").map { it.toInt() }
-	val que = java.util.PriorityQueue<Triple<Int, Int, Boolean>>(compareBy { !it.third })
-	val P = Array(n) {StringBuilder(br.readLine())}
 
-	for (i in 0 until n) {
-		for (j in 0 until m) {
-			when (P[i][j]) {
-				'J' -> {
-					que.add(Triple(i, j, false))
-				}
-				'F' -> que.add(Triple(i, j, true))
-			}
+const val YES = "YES"
+const val NO = "NO"
+fun isSlump(s: String): Boolean {
+	if (s.length < 3 || s[0] != 'D' && s[0] != 'E' || s[1] != 'F') return false
+	var fIdx = 2
+	for (i in 2 until s.length) {
+		if (s[i] == 'F') fIdx = i + 1
+		else break
+	}
+	return s.substring(fIdx, s.length).let {
+		it == "G" || isSlump(it)
+	}
+}
+fun isSlimp(s: String): Boolean {
+	if (s.length < 2 || s[0] != 'A') return false
+	if (s.length == 2) return s[1] == 'H'
+	if (s.last() != 'C') return false
+	if (s[1] == 'B') return isSlimp(s.substring(2, s.length - 1))
+	return isSlump(s.substring(1, s.length - 1))
+}
+fun solution(str: String): String {
+	var ret = NO
+	for (i in 1 until str.length) {
+		if (isSlimp(str.substring(0, i)) && isSlump(str.substring(i))) {
+			ret = YES
+			break
 		}
 	}
-	val tmp = ArrayDeque<Triple<Int, Int, Boolean>>()
-	var ret = 0
-	while (que.isNotEmpty()) {
-		++ret
-		for (tc in que.indices) {
-			val cur = que.poll()
-			for (i in dr.indices) {
-				val y = cur.first + dr[i]
-				val x = cur.second + dc[i]
-				if (y in 0 until n && x in 0 until m) {
-					if (cur.third && P[y][x] != '#' && P[y][x] != 'F') {
-						P[y][x] = 'F'
-						tmp.add(Triple(y, x, true))
-					} else if (!cur.third && P[y][x] == '.') {
-						P[y][x] = 'J'
-						tmp.add(Triple(y, x, false))
-					}
-				} else if (!cur.third) {
-					println(ret)
-					return
-				}
-			}
-		}
-		while (tmp.isNotEmpty()) que.add(tmp.removeFirst())
+	return ret
+}
+fun main() {
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
+	bw.write("SLURPYS OUTPUT\n")
+	repeat(readLine()!!.toInt()) {
+		bw.write(solution(readLine()!!) + "\n")
 	}
-	println("IMPOSSIBLE")
+	bw.write("END OF OUTPUT\n")
+	bw.flush()
 }
