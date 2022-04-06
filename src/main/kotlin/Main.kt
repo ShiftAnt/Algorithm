240d5185796a94338b63f5ffa3ddcb4f8ae77c7a
김지웅 <kjw4611@gmail.com>
2022-04-06 12:16:11 +0900

    [BJ 2266] 금고 테스트

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
+import kotlin.math.*
+
+fun dfs(n: Int, tmp: Int): Int {
+	val k = if (tmp > n) n else tmp
+
+	if (P[n][k] != Int.MAX_VALUE) return P[n][k]
+	if (n == 0) return 0
+	if (k == 1) return n
+
+	for (i in 1..n) P[n][k] = min(P[n][k], max(dfs(n - i, k), dfs(i - 1, k - 1)) + 1)
+	return P[n][k]
+}
+var P = arrayOf<IntArray>()
 fun main() {
-	val regex = Regex("((100+1+)|(01))+")
-	repeat(readLine()!!.toInt()) {
-		println(if (regex.matches(readLine()!!)) "YES" else "NO")
-	}
+	val (n, k) = readLine()!!.split(" ").map { it.toInt() }
+	P = Array(n + 1) { IntArray(it + 1) {Int.MAX_VALUE} }
+
+	println(dfs(n, k))
 }
