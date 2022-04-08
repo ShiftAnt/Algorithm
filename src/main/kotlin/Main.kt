67053f41bc6a5d808b121f353b96969b3a745fc4
김지웅 <kjw4611@gmail.com>
2022-04-08 13:25:28 +0900

    [BJ 2632] 피자판매

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,30 @@
-import kotlin.math.*
-
-val dr = arrayOf(-1, 0, 0, 1)
-val dc = arrayOf(0, -1, 1, 0)
+import java.io.*
 fun main() {
-	val (n, m) = readLine()!!.split(" ").map { it.toInt() }
-	val P = Array(n) { readLine()!!.toCharArray().map { it - '0' }.toIntArray() }
-	var ret = 0
+	val br = BufferedReader(InputStreamReader(System.`in`))
+	val s = br.readLine().toInt()
+	val (m, n) = br.readLine().split(" ").map { it.toInt() }
+	val A = arrayOf(IntArray(m) {br.readLine().toInt()}, IntArray(n) {br.readLine().toInt()})
 
-	fun bfs(y: Int, x: Int) {
-		val que = ArrayList<Pair<Int, Int>>()
-		val vstd = Array(n) {BooleanArray(m)}
-		vstd[y][x] = true
-		que.add(y to x)
-
-		var isFail = false
-		var idx = -1
-		var mi = Int.MAX_VALUE
-		while(++idx < que.size) {
-			val cur = que[idx]
-			for (i in dr.indices) {
-				val a = cur.first + dr[i]
-				val b = cur.second + dc[i]
-				if (vstd[a][b]) continue
-				vstd[a][b] = true
-				if (P[a][b] == -1) {
-					isFail = true
-					continue
-				}
-				if (P[y][x] == P[a][b]) que.add(a to b)
-				else mi = min(mi, P[a][b])
+	val P = Array(2) {IntArray(s + 1)}
+	P[0][0] = 1
+	P[1][0] = 1
+	repeat(2) { tc ->
+		A[tc].sum().let { if (it <= s) ++P[tc][it] }
+		for (i in A[tc].indices) {
+			var cur = i
+			val end = if (i == 0) A[tc].size - 1 else i - 1
+			var num = 0
+			while (cur != end) {
+				num += A[tc][cur]
+				if (num > s) break
+				++P[tc][num]
+				if (++cur == A[tc].size) cur = 0
 			}
 		}
-		if (isFail) {
-			que.forEach { P[it.first][it.second] = -1 }
-			return
-		}
-		ret += que.size * (mi - P[y][x])
-		que.forEach { P[it.first][it.second] = mi }
 	}
-
-	for (tc in 1 until 9) {
-		for (i in 0 until n) {
-			for (j in 0 until m) {
-				if ((i == 0 || i == n - 1 || j == 0 || j == m - 1) && P[i][j] == tc) P[i][j] = -1
-			}
-		}
-		for (i in 1 until n - 1) {
-			for (j in 0 until m - 1) {
-				if (P[i][j] == tc) {
-					bfs(i, j)
-				}
-			}
-		}
+	var ret = 0L
+	for (i in 0..s) {
+		ret += P[0][i] * P[1][s - i]
 	}
 	println(ret)
 }
