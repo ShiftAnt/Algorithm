882717c71946a766852c270cbcce58848403cb22
김지웅 <kjw4611@gmail.com>
2022-04-12 13:00:06 +0900

    [BJ 16967] 배열 복원하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,13 @@
-import kotlin.math.max
-
 fun main() {
-	val n = readLine()!!.toInt()
-	val P = readLine()!!.split(" ").map { it.toInt() }
-	val al = Array(n) {ArrayList<Int>()}
-	repeat(n - 1) {
-		val (a, b) = readLine()!!.split(" ").map { it.toInt() - 1}
-		al[a] += b
-		al[b] += a
-	}
-	val vstd = BooleanArray(n)
-	val sub = Array(n) { 0 to 0 }
-	fun dfs(idx: Int): Pair<Int, Int> {
-		vstd[idx] = true
-		var exc = 0
-		var inc = P[idx]
-		for (nxt in al[idx]) {
-			if (vstd[nxt]) continue
-			dfs(nxt).let {
-				exc += max(it.first, it.second)
-				inc += it.first
-			}
-		}
-		sub[idx] = exc to inc
-		return exc to inc
-	}
-	dfs(0).let {
-		println(max(it.first, it.second))
-	}
-	vstd.fill(false)
-	val ret = ArrayList<Int>()
-	fun indi(idx: Int, prev: Boolean = false) {
-		vstd[idx] = true
-		if (!prev && sub[idx].second  >= sub[idx].first) ret += idx + 1
+	val (h, w, x, y) = readLine()!!.split(" ").map { it.toInt() }
 
-		for (nxt in al[idx]) {
-			if (vstd[nxt]) continue
-			if (!prev && sub[idx].second >= sub[idx].first) indi(nxt, true)
-			else indi(nxt, false)
+	val P = Array(h + x) { readLine()!!.split(" ").map { it.toInt() }.toIntArray() }
+
+	for (i in 0 until h) {
+		for (j in 0 until w) {
+			P[i + x][j + y] -= P[i][j]
+			print("${P[i][j]} ")
 		}
+		print("\n")
 	}
-	indi(0, false)
-	ret.sort()
-	println(ret.joinToString(" "))
 }
