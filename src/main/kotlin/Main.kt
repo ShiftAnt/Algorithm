2410a1f6247229032fb9e22b2992d42c0718ff40
김지웅 <kjw4611@gmail.com>
2022-04-14 12:29:14 +0900

    [BJ 10217] KCM Travel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,56 +1,41 @@
 import java.io.*
-import kotlin.math.abs
-
-fun main() {
-	val br = BufferedReader(InputStreamReader(System.`in`))
-
-	val n = br.readLine().toInt()
-	val dif = 0 to 0
-	val P = Array(3) {Array(n) {dif} }
-
-	for (i in 0 until n) {
-		val st = java.util.StringTokenizer(br.readLine())
-		for (j in 0 until 3) {
-			P[j][i] = st.nextToken().toInt() to i
-		}
+const val INF = Int.MAX_VALUE / 2
+fun solution(n: Int, m: Int, k: Int) {
+	val al = Array(n + 1) {ArrayList<Triple<Int, Int, Int>>()}
+	repeat(k) {
+		val (a, b, c, d) = br.readLine().split(" ").map { it.toInt() }
+		al[a] += Triple(b, c, d)
 	}
-	for (i in 0 until 3) P[i].sortBy { it.first }
-	val que = java.util.PriorityQueue<Triple<Int, Int, Int>>(compareBy{it.first})
-	for (i in 0 until 3) {
-		for (j in 0 until n - 1) {
-			que += Triple(abs(P[i][j].first - P[i][j + 1].first), P[i][j].second, P[i][j + 1].second)
-		}
-	}
-	val djs = IntArray(n) {it}
-	val cnt = IntArray(n) {1}
+	val P = Array(n + 1) {IntArray(m + 1) {INF} }
+	P[1][0] = 0
 
-	fun dis(idx: Int, set: Int = -1): Int {
-		var cur = idx
+	val que = java.util.PriorityQueue<Triple<Int, Int, Int>>(compareBy {it.third})
+	que += Triple(1, 0, 0)
 
-		while (cur != djs[cur]) {
-			val tmp = cur
-			cur = djs[cur]
-			if (set != -1) djs[tmp] = set
-		}
-		if (set != -1) djs[cur] = set
-		return cur
-	}
-	var ret = 0
 	while (que.isNotEmpty()) {
 		val cur = que.poll()
-		val da = dis(cur.second)
-		val db = dis(cur.third)
-		if (da == db) continue
-		ret += cur.first
-		if (cnt[da] > cnt[db]) {
-			cnt[da] += cnt[db]
-			dis(cur.second, da)
-			dis(cur.third, da)
-		} else {
-			cnt[db] += cnt[da]
-			dis(cur.second, db)
-			dis(cur.third, db)
+		if (cur.first == n) {
+			bw.write("${cur.third}\n")
+			return
+		}
+		if (cur.third > P[cur.first][cur.second]) continue
+		for (nxt in al[cur.first]) {
+			val nxtFee = cur.second + nxt.second
+			val nxtVal = cur.third + nxt.third
+			if (nxtFee > m || P[nxt.first][nxtFee] <= nxtVal) continue
+			P[nxt.first][nxtFee] = nxtVal
+			que += Triple(nxt.first, nxtFee, nxtVal)
 		}
 	}
-	println(ret)
+	bw.write("Poor KCM\n")
+}
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() {
+	repeat(br.readLine().toInt()) {
+		br.readLine().split(" ").map { it.toInt() }.let {
+			solution(it[0], it[1], it[2])
+		}
+	}
+	bw.flush()
 }
