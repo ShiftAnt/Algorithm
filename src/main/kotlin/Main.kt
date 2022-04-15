0f3fbd4b7e15b75cb8adfd04e8cbcd51a5ca8dc5
김지웅 <kjw4611@gmail.com>
2022-04-15 15:28:32 +0900

    [BJ 1826] 연료 채우기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,39 @@
-import java.io.*
-const val INF = Int.MAX_VALUE / 2
-fun solution(n: Int, m: Int, k: Int) {
-	val al = Array(n + 1) {ArrayList<Triple<Int, Int, Int>>()}
-	repeat(k) {
-		val (a, b, c, d) = br.readLine().split(" ").map { it.toInt() }
-		al[a] += Triple(b, c, d)
-	}
-	val P = Array(n + 1) {IntArray(m + 1) {INF} }
-	P[1][0] = 0
-
-	val que = java.util.PriorityQueue<Triple<Int, Int, Int>>(compareBy {it.third})
-	que += Triple(1, 0, 0)
-
-	while (que.isNotEmpty()) {
-		val cur = que.poll()
-		if (cur.first == n) {
-			bw.write("${cur.third}\n")
-			return
-		}
-		if (cur.third > P[cur.first][cur.second]) continue
-		for (nxt in al[cur.first]) {
-			val nxtFee = cur.second + nxt.second
-			val nxtVal = cur.third + nxt.third
-			if (nxtFee > m || P[nxt.first][nxtFee] <= nxtVal) continue
-			P[nxt.first][nxtFee] = nxtVal
-			que += Triple(nxt.first, nxtFee, nxtVal)
-		}
-	}
-	bw.write("Poor KCM\n")
-}
-val br = BufferedReader(InputStreamReader(System.`in`))
-val bw = BufferedWriter(OutputStreamWriter(System.out))
 fun main() {
-	repeat(br.readLine().toInt()) {
-		br.readLine().split(" ").map { it.toInt() }.let {
-			solution(it[0], it[1], it[2])
+	val n = readLine()!!.toInt()
+
+	val oils = ArrayList<Pair<Int, Int>>()
+
+	repeat(n) {
+		oils += readLine()!!.split(" ").map { it.toInt() }.let {
+			it[0] to it[1]
 		}
 	}
-	bw.flush()
+	val (l, p) = readLine()!!.split(" ").map { it.toInt() }
+	oils += l to 0
+	oils.sortBy { it.first }
+
+	val pq = java.util.PriorityQueue<Int>(compareByDescending { it })
+	var ret = -1
+	var sub = 0
+	var a = 0
+	var b = p
+	for (i in oils.indices) {
+		val cur = oils[i]
+
+		while (cur.first - a > b && pq.isNotEmpty()) {
+			b += pq.poll()
+			++sub
+		}
+		if (cur.first - a > b) break
+
+		pq += cur.second
+		b -= (cur.first - a)
+		a = cur.first
+		if (a == l) {
+			ret = sub
+			break
+		}
+	}
+
+	println(ret)
 }
