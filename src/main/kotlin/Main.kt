dbc71ec5de256d0ac6676fe17ca118064a7da7e1
김지웅 <kjw4611@gmail.com>
2022-04-16 06:05:54 +0900

    [BJ 4342] 유클리드 게임

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,15 @@
 fun main() {
-	val n = readLine()!!.toInt()
-
-	val oils = ArrayList<Pair<Int, Int>>()
-
-	repeat(n) {
-		oils += readLine()!!.split(" ").map { it.toInt() }.let {
-			it[0] to it[1]
+	while (true) {
+		var (a, b) = readLine()!!.split(" ").map { it.toInt() }
+		if (a == 0) break
+		var isA = true
+		while (true) {
+			if (a < b) a = b.also { b = a }
+			if (a / b > 1) break
+			a -= b
+			if (a == 0) break
+			isA = !isA
 		}
+		print("${if (isA) 'A' else 'B'} wins\n")
 	}
-	val (l, p) = readLine()!!.split(" ").map { it.toInt() }
-	oils += l to 0
-	oils.sortBy { it.first }
-
-	val pq = java.util.PriorityQueue<Int>(compareByDescending { it })
-	var ret = -1
-	var sub = 0
-	var a = 0
-	var b = p
-	for (i in oils.indices) {
-		val cur = oils[i]
-
-		while (cur.first - a > b && pq.isNotEmpty()) {
-			b += pq.poll()
-			++sub
-		}
-		if (cur.first - a > b) break
-
-		pq += cur.second
-		b -= (cur.first - a)
-		a = cur.first
-		if (a == l) {
-			ret = sub
-			break
-		}
-	}
-
-	println(ret)
 }
