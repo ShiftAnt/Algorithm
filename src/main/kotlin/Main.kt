a9c5a2c6293bf1b393f440a0a30309bb2f8cb8af
김지웅 <kjw4611@gmail.com>
2022-04-17 06:53:11 +0900

    [BJ 2110] 공유기 설치

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,33 @@
 fun main() {
-	while (true) {
-		var (a, b) = readLine()!!.split(" ").map { it.toInt() }
-		if (a == 0) break
-		var isA = true
-		while (true) {
-			if (a < b) a = b.also { b = a }
-			if (a / b > 1) break
-			a -= b
-			if (a == 0) break
-			isA = !isA
+	val (n, c) = readLine()!!.split(" ").map { it.toInt() }
+	val P = Array(n) { readLine()!!.toLong() }.sorted()
+
+	var a = 1L
+	var b = P[n - 1] - P[0]
+	var ret = 0L
+	while (a <= b) {
+		val mid = (a + b) / 2
+		var prev = P[0]
+		var idx = 1
+		var isUp = true
+		loop@
+		for (tc in 0 until c - 2) {
+			if (idx == n - 1) {
+				isUp = false
+				break
+			}
+			while (P[idx] - prev < mid) if (++idx == n - 1) {
+				isUp = false
+				break@loop
+			}
+			prev = P[idx++]
 		}
-		print("${if (isA) 'A' else 'B'} wins\n")
+		if (isUp && P[n - 1] - P[idx - 1] < mid) isUp = false
+
+		if (isUp) {
+			ret = mid
+			a = mid + 1
+		} else b = mid - 1
 	}
+	println(ret)
 }
