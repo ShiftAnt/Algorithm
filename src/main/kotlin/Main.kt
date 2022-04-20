e738ae7a4fbbbcd5142225f29e44cabe853098e1
김지웅 <kjw4611@gmail.com>
2022-04-20 12:14:38 +0900

    [BJ 23288] 주사위 굴리기 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,111 +1,83 @@
 var n = 0
 var m = 0
-var center = 0
+var k = 0
 var P = arrayOf<IntArray>()
-var route = ArrayList<Pair<Int, Int>>()
+var dir = 0
+val dr = arrayOf(0, 1, 0, -1)
+val dc = arrayOf(1, 0, -1, 0)
+val dice = IntArray(6) {it + 1}
+val lr = intArrayOf(0, 2, 5, 3)
+val ud = intArrayOf(0, 4, 5, 1)
+val cur = intArrayOf(0, 0)
+var dp = arrayOf<IntArray>()
 var ret = 0
-fun move(d: Int, s: Int) {
-	for (i in 1..s) {
-		val y = center + dr[d] * i
-		val x = center + dc[d] * i
-		if (y !in 0 until n || x !in 0 until n) break
-		P[y][x] = 0
-	}
-	pull()
-	while (del()) {
-		pull()
-	}
-	ext()
-}
-fun delMarble(y: Int, x: Int) {
-	ret += P[y][x]
-	P[y][x] = 0
-}
-val dr = arrayOf(-1, 1, 0, 0)
-val dc = arrayOf(0, 0, -1, 1)
-val di = arrayOf(3, 1, 2, 0)
-fun initRoute() {
-	val vstd = Array(n) {BooleanArray(n)}
+fun move() {
+	val ny = cur[0] + dr[dir]
+	val nx = cur[1] + dc[dir]
+	if (ny !in 0 until n || nx !in 0 until m) dir = (dir + 2) % 4
+	cur[0] += dr[dir]
+	cur[1] += dc[dir]
 
-	var d = 0
-	var y = 0
-	var x = 0
-
-	while (y != center || x != center) {
-		route += y to x
-		vstd[y][x] = true
-		val ny = y + dr[di[d]]
-		val nx = x + dc[di[d]]
-		if (ny !in 0 until n || nx !in 0 until n || vstd[ny][nx]) d = (d + 1) % 4
-		y += dr[di[d]]
-		x += dc[di[d]]
+	when (dir) {
+		0 -> moveDice(lr)
+		1 -> moveDice(ud)
+		2 -> moveDice(lr, true)
+		3 -> moveDice(ud, true)
 	}
-	route.reverse()
+	val sub = dice[5] - P[cur[0]][cur[1]]
+
+	if (sub > 0) {
+		dir = (dir + 1) % 4
+	}
+	else if (sub < 0) {
+		dir = (dir + 3) % 4
+	}
+	ret += dp[cur[0]][cur[1]]
 }
-fun del(): Boolean {
-	var y = route[0].first
-	var x = route[0].second
-	var cur = P[y][x]
-	var num = 1
-	var isDel = false
-	for (i in 1 until route.size) {
-		y = route[i].first
-		x = route[i].second
-		if (cur == P[y][x]) ++num
-		else {
-			if (delNums(i, num)) isDel = true
-			cur = P[y][x]
-			num = 1
+
+
+fun moveDice(type: IntArray, isR: Boolean = false) {
+	val ts = type.size
+	val tmp = ArrayDeque<Int>()
+	repeat(ts) {tmp.addLast(dice[type[it]])}
+	if (!isR) tmp.addFirst(tmp.removeLast())
+	else tmp.addLast(tmp.removeFirst())
+
+	for (i in 0 until ts) {
+		dice[type[i]] = tmp[i]
+	}
+}
+fun bfs(y: Int, x: Int) {
+	dp[y][x] = 0
+	val que = ArrayList<Pair<Int, Int>>()
+	que += y to x
+	var idx = -1
+
+	while (++idx < que.size) {
+		val cur = que[idx]
+
+		for (i in dr.indices) {
+			val a = cur.first + dr[i]
+			val b = cur.second + dc[i]
+			if (a in 0 until n && b in 0 until m && dp[a][b] == -1 && P[a][b] == P[y][x]) {
+				dp[a][b] = 0
+				que += a to b
+			}
 		}
-		if (cur == 0) break
 	}
-	if (cur != 0) if (delNums(route.size, num)) isDel = true
-	return isDel
-}
-fun delNums(idx: Int, num: Int): Boolean {
-	if (num < 4) return false
-	for (i in idx - num until idx) {
-		delMarble(route[i].first, route[i].second)
+	val siz = que.size
+	for (ele in que) {
+		dp[ele.first][ele.second] = P[y][x] * siz
 	}
-	return true
 }
-fun ext() {
-	var y = route[0].first
-	var x = route[0].second
-	var cur = P[y][x]
-	if (cur == 0) return
-	var num = 1
-	val nums = ArrayList<Int>()
-	for (i in 1 until route.size) {
-		y = route[i].first
-		x = route[i].second
-		if (cur == P[y][x]) ++num
-		else {
-			nums += num
-			nums += cur
-			cur = P[y][x]
-			num = 1
+
+fun initScore() {
+	dp = Array(n) {IntArray(m) {-1} }
+
+	for (i in 0 until n) {
+		for (j in 0 until m) {
+			if (dp[i][j] == -1) bfs(i, j)
 		}
-		if (P[y][x] == 0) break
-	}
-	if (cur != 0) {
-		nums += num
-		nums += cur
-	}
-	for (i in route.indices) {
-		P[route[i].first][route[i].second] = if (i < nums.size) nums[i] else 0
-	}
-}
-
-fun pull() {
-	val nums = ArrayList<Int>()
-
-	for (r in route) {
-		if (P[r.first][r.second] != 0) nums += P[r.first][r.second]
-		P[r.first][r.second] = 0
-	}
-	for (i in nums.indices) {
-		P[route[i].first][route[i].second] = nums[i]
 	}
 }
 
@@ -113,15 +85,12 @@ fun main() {
 	readLine()!!.split(" ").map { it.toInt() }.let {
 		n = it[0]
 		m = it[1]
+		k = it[2]
 	}
-	center = n / 2
 	P = Array(n) { readLine()!!.split(" ").map { it.toInt() }.toIntArray() }
-
-	initRoute()
-	repeat(m) {
-		readLine()!!.split(" ").map { it.toInt() }.let {
-			move(it[0] - 1, it[1])
-		}
+	initScore()
+	repeat(k) {
+		move()
 	}
 	println(ret)
 }
