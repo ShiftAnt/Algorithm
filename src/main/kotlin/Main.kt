9ed94d3bdcc7489717e13b7f1a0531cd85f7017d
김지웅 <kjw4611@gmail.com>
2022-04-23 10:09:59 +0900

    [BJ 1655] 가운데를 말해요

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,35 @@
 import java.io.*
-import kotlin.math.*
-
 fun main() {
 	val br = BufferedReader(InputStreamReader(System.`in`))
-	val (d, n) = br.readLine().split(" ").map { it.toInt() }
-	val oven = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-	val pz = br.readLine().split(" ").map { it.toInt() }
-	var idx = 0
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
+	val lPq = java.util.PriorityQueue<Int>(compareByDescending { it })
+	val rPq = java.util.PriorityQueue<Int>()
 
-	for (i in 1 until d) {
-		oven[i] = min(oven[i - 1], oven[i])
-	}
-	for (i in d - 1 downTo 0) {
-		if (pz[idx] <= oven[i]) {
-			if (idx == n - 1) {
-				println(i + 1)
-				return
+	for (tc in 0 until br.readLine().toInt()) {
+		val num = br.readLine().toInt()
+		val ls = lPq.size
+		val rs = rPq.size
+		if (lPq.isEmpty()) {
+			lPq += num
+		} else {
+			val lt = lPq.peek()
+			if (ls == rs) {
+				val rt = rPq.peek()
+				if (num <= rt) lPq += num
+				else {
+					lPq += rPq.poll()
+					rPq += num
+				}
+			} else {
+				if (num >= lt) {
+					rPq += num
+				} else {
+					rPq += lPq.poll()
+					lPq += num
+				}
 			}
-			++idx
 		}
+		bw.write("${lPq.peek()}\n")
 	}
-	println(0)
+	bw.flush()
 }
