88b591f5a4c01aa06db113fd53edc72a05ea1685
김지웅 <kjw4611@gmail.com>
2022-04-28 10:42:14 +0900

    [BJ 1890] 점프

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,24 @@
-import java.math.*
-
+val dr = arrayOf(0, 1)
+val dc = arrayOf(1, 0)
 fun main() {
-	var a: BigDecimal
-	var b: Int
-	readLine()!!.split(" ").let {
-		a = BigDecimal(it[0])
-		b = it[1].toInt()
+	val n = readLine()!!.toInt()
+	val P = Array(n) { readLine()!!.split(" ").map { it.toInt() }.toIntArray() }
+
+	val dp = Array(n) {LongArray(n)}
+
+	dp[0][0] = 1
+
+	for (i in 0 until n) {
+		for (j in 0 until n) {
+			if (P[i][j] == 0) continue
+			for (k in dr.indices) {
+				val a = i + dr[k] * P[i][j]
+				val b = j + dc[k] * P[i][j]
+				if (a in 0 until n && b in 0 until n) {
+					dp[a][b] += dp[i][j]
+				}
+			}
+		}
 	}
-	var sub = a
-	var ret = BigDecimal(1.0)
-	while (b != 0) {
-		if (b % 2 != 0) ret *= sub
-		sub *= sub
-		b /= 2
-	}
-	println(ret.toPlainString())
+	println(dp[n - 1][n - 1])
 }
