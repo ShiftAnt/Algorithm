ee1ec63579aa4371f9b21b62a00a1de0749b4831
김지웅 <kjw4611@gmail.com>
2022-04-29 10:16:17 +0900

    [BJ 4485] 녹색 옷 입은 애가 젤다지?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,39 @@
-val dr = arrayOf(0, 1)
-val dc = arrayOf(1, 0)
-fun main() {
-	val n = readLine()!!.toInt()
-	val P = Array(n) { readLine()!!.split(" ").map { it.toInt() }.toIntArray() }
+var n = 0
+lateinit var P: Array<IntArray>
+lateinit var dp: Array<IntArray>
+const val INF = Int.MAX_VALUE
+val dr = arrayOf(-1, 0, 0, 1)
+val dc = arrayOf(0, -1, 1, 0)
 
-	val dp = Array(n) {LongArray(n)}
-
-	dp[0][0] = 1
-
-	for (i in 0 until n) {
-		for (j in 0 until n) {
-			if (P[i][j] == 0) continue
-			for (k in dr.indices) {
-				val a = i + dr[k] * P[i][j]
-				val b = j + dc[k] * P[i][j]
-				if (a in 0 until n && b in 0 until n) {
-					dp[a][b] += dp[i][j]
-				}
-			}
+fun solution(): Int {
+	val que = java.util.PriorityQueue<Triple<Int, Int, Int>>(compareBy { it.third })
+	dp[0][0] = P[0][0]
+	que += Triple(0, 0, P[0][0])
+	while (que.isNotEmpty()) {
+		val cur = que.poll()
+		val y = cur.first
+		val x = cur.second
+		if (dp[y][x] < cur.third) continue
+		if (y == n - 1 && x == n - 1) break
+		for (i in dr.indices) {
+			val a = y + dr[i]
+			val b = x + dc[i]
+			if (a !in 0 until n || b !in 0 until n || dp[a][b] <= dp[y][x] + P[a][b]) continue
+			dp[a][b] = dp[y][x] + P[a][b]
+			que += Triple(a, b, dp[a][b])
 		}
 	}
-	println(dp[n - 1][n - 1])
+	return dp[n - 1][n - 1]
+}
+
+fun main() {
+	var tc = 0
+	while (true) {
+		++tc
+		n = readLine()!!.toInt()
+		if (n == 0) break
+		P = Array(n) { readLine()!!.split(" ").map { it.toInt() }.toIntArray() }
+		dp = Array(n) { IntArray(n) {INF} }
+		println("Problem $tc: ${solution()}")
+	}
 }
