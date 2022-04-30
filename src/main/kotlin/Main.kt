fad0234474d5173fd21b5ce5aca8ffb04029b82e
김지웅 <kjw4611@gmail.com>
2022-04-30 10:17:03 +0900

    [BJ 2493] 탑

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,20 @@
-var n = 0
-lateinit var P: Array<IntArray>
-lateinit var dp: Array<IntArray>
-const val INF = Int.MAX_VALUE
-val dr = arrayOf(-1, 0, 0, 1)
-val dc = arrayOf(0, -1, 1, 0)
-
-fun solution(): Int {
-	val que = java.util.PriorityQueue<Triple<Int, Int, Int>>(compareBy { it.third })
-	dp[0][0] = P[0][0]
-	que += Triple(0, 0, P[0][0])
-	while (que.isNotEmpty()) {
-		val cur = que.poll()
-		val y = cur.first
-		val x = cur.second
-		if (dp[y][x] < cur.third) continue
-		if (y == n - 1 && x == n - 1) break
-		for (i in dr.indices) {
-			val a = y + dr[i]
-			val b = x + dc[i]
-			if (a !in 0 until n || b !in 0 until n || dp[a][b] <= dp[y][x] + P[a][b]) continue
-			dp[a][b] = dp[y][x] + P[a][b]
-			que += Triple(a, b, dp[a][b])
-		}
-	}
-	return dp[n - 1][n - 1]
-}
+import java.io.*
 
 fun main() {
-	var tc = 0
-	while (true) {
-		++tc
-		n = readLine()!!.toInt()
-		if (n == 0) break
-		P = Array(n) { readLine()!!.split(" ").map { it.toInt() }.toIntArray() }
-		dp = Array(n) { IntArray(n) {INF} }
-		println("Problem $tc: ${solution()}")
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
+	val n = readLine()!!.toInt()
+	val st = java.util.StringTokenizer(readLine()!!)
+	val stack = java.util.Stack<Pair<Int, Int>>()
+	for (i in 0 until n) {
+		val cur = st.nextToken().toInt()
+
+		while (stack.isNotEmpty()) {
+			if (stack.peek().second < cur) {
+				stack.pop()
+			} else break
+		}
+		bw.write(if (stack.isEmpty()) "0 " else "${stack.peek().first + 1} ")
+		stack += i to cur
 	}
+	bw.flush()
 }
