2ce9f324fdf204063008ef295556f270790188dd
김지웅 <kjw4611@gmail.com>
2022-05-01 07:23:46 +0900

    [BJ 11656] 접미사 배열

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,9 @@
-import java.io.*
-
 fun main() {
-	val bw = BufferedWriter(OutputStreamWriter(System.out))
-	val n = readLine()!!.toInt()
-	val st = java.util.StringTokenizer(readLine()!!)
-	val stack = java.util.Stack<Pair<Int, Int>>()
-	for (i in 0 until n) {
-		val cur = st.nextToken().toInt()
-
-		while (stack.isNotEmpty()) {
-			if (stack.peek().second < cur) {
-				stack.pop()
-			} else break
+	readLine()!!.let { str ->
+		arrayListOf<String>().let { lst ->
+			for (i in str.indices) lst += str.substring(i)
+			lst.sort()
+			println(lst.joinToString("\n"))
 		}
-		bw.write(if (stack.isEmpty()) "0 " else "${stack.peek().first + 1} ")
-		stack += i to cur
 	}
-	bw.flush()
 }
