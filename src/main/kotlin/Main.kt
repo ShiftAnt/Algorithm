0b3775f0f8ec5fe74fec708841502af87dd3cff2
김지웅 <kjw4611@gmail.com>
2022-05-02 09:09:30 +0900

    [BJ 6198] 옥상 정원 꾸미기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
 fun main() {
-	readLine()!!.let { str ->
-		arrayListOf<String>().let { lst ->
-			for (i in str.indices) lst += str.substring(i)
-			lst.sort()
-			println(lst.joinToString("\n"))
+	val n = readLine()!!.toInt()
+	val P = IntArray(n) { readLine()!!.toInt() }
+
+	val st = java.util.Stack<Pair<Int, Int>>()
+	var ret = 0L
+	for (i in n - 1 downTo 0) {
+		val cur = i to P[i]
+
+		while (st.isNotEmpty() && cur.second > st.peek().second) {
+			st.pop()
 		}
+		ret += (if (st.isEmpty()) n else st.peek().first) - 1 - cur.first
+
+		st += cur
 	}
+	println(ret)
 }
