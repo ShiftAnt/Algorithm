4e43cab98a72a2b0792bee7cb8967f229771f030
김지웅 <kjw4611@gmail.com>
2022-05-06 11:47:13 +0900

    [BJ 1022] 소용돌이 예쁘게 출력하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,40 @@
+import kotlin.math.*
+fun find(r: Int, c: Int): Int {
+	val n = max(abs(r), abs(c))
+
+	val lt = 4 * n * n + 1
+	val rd = lt + 4 * n
+
+	if (r == n) return rd - (n - c)
+	if (c == n) return lt - 2 * n - (r + n)
+	if (r == -n) return lt - (c + n)
+	return lt + (r + n)
+}
+
+fun blank(str: String, num: Int) =  StringBuilder().also { sb ->
+	repeat(num - str.length) {
+		sb.append(" ")
+	}
+	sb.append(str)
+}.toString()
+
 fun main() {
-	val (n, k) = readLine()!!.split(" ").map { it.toInt() }
-	val P = Array(n) {IntArray(n)}
-	repeat(k) {
-		readLine()!!.split(" ").map { it.toInt() - 1 }.let {
-			P[it[0]][it[1]] = -1
-			P[it[1]][it[0]] = 1
+	val (r1, c1, r2, c2) = readLine()!!.split(" ").map { it.toInt() }
+	val n = r2 - r1 + 1
+	val m = c2 - c1 + 1
+	val P = Array(n) {IntArray(m)}
+	var mx = 0
+	for (i in r1..r2) {
+		for (j in c1..c2) {
+			P[i - r1][j - c1] = find(i, j)
+			mx = max(mx, P[i - r1][j - c1])
 		}
 	}
-	for (m in 0 until n) {
-		for (i in 0 until n) {
-			for (j in 0 until n) {
-				if (P[i][m] == P[m][j] && P[i][m] != 0) P[i][j] = P[i][m]
-			}
+	mx = mx.toString().length
+	for (i in 0 until n) {
+		for (j in 0 until m) {
+			print("${blank(P[i][j].toString(), mx)} ")
 		}
-	}
-	repeat(readLine()!!.toInt()) {
-		val (a, b) = readLine()!!.split(" ").map { it.toInt() - 1 }
-		println(P[a][b])
+		print("\n")
 	}
 }
