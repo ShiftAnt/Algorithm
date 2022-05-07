a1cbfe5e28fd1e9c5112dddb9732b1648e85eb02
김지웅 <kjw4611@gmail.com>
2022-05-07 18:16:00 +0900

    [BJ 15235] Olympiad Pizza

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,17 @@
-import kotlin.math.*
-fun find(r: Int, c: Int): Int {
-	val n = max(abs(r), abs(c))
-
-	val lt = 4 * n * n + 1
-	val rd = lt + 4 * n
-
-	if (r == n) return rd - (n - c)
-	if (c == n) return lt - 2 * n - (r + n)
-	if (r == -n) return lt - (c + n)
-	return lt + (r + n)
-}
-
-fun blank(str: String, num: Int) =  StringBuilder().also { sb ->
-	repeat(num - str.length) {
-		sb.append(" ")
-	}
-	sb.append(str)
-}.toString()
-
 fun main() {
-	val (r1, c1, r2, c2) = readLine()!!.split(" ").map { it.toInt() }
-	val n = r2 - r1 + 1
-	val m = c2 - c1 + 1
-	val P = Array(n) {IntArray(m)}
-	var mx = 0
-	for (i in r1..r2) {
-		for (j in c1..c2) {
-			P[i - r1][j - c1] = find(i, j)
-			mx = max(mx, P[i - r1][j - c1])
+	val n = readLine()!!.toInt()
+	val st = java.util.StringTokenizer(readLine())
+	val P = IntArray(n) {st.nextToken().toInt()}
+
+	val ret = IntArray(n)
+	var sub = 0
+	while (P.sumOf { it } != 0) {
+		for (i in P.indices) {
+			if (P[i] != 0) {
+				++sub
+				if (--P[i] == 0) ret[i] = sub
+			}
 		}
 	}
-	mx = mx.toString().length
-	for (i in 0 until n) {
-		for (j in 0 until m) {
-			print("${blank(P[i][j].toString(), mx)} ")
-		}
-		print("\n")
-	}
+	println(ret.joinToString(" "))
 }
