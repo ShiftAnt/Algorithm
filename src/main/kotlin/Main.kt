6739a814fef2528ad911b8a2a7f55ede0e3023e0
김지웅 <kjw4611@gmail.com>
2022-05-15 11:49:02 +0900

    [BJ 1026] 보물

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,11 @@
-class Fraction(
-	val a: Int,
-	val b: Int
-) {
-	operator fun compareTo(other: Fraction) =
-		(this.a.toLong() * other.b - other.a.toLong() * this.b).let {
-			if (it > 0) 1 else if (it < 0) -1 else 0
-		}
-}
-
 fun main() {
 	val n = readLine()!!.toInt()
-
-	val P = readLine()!!.split(" ").map { it.toInt() }
-	val ret = IntArray(n)
-	for (i in 0 until n) {
-		var mx = Fraction(0, 0)
-		for (r in i + 1 until n) {
-			val cur = Fraction(P[r] - P[i], r - i)
-			mx = if (r == i + 1) {
-				cur
-			} else if (cur > mx) cur
-			else continue
-			if (cur.a > 0) {
-				++ret[i]
-				++ret[r]
-			}
-		}
-		for (l in i - 1 downTo 0) {
-			val cur = Fraction(P[l] - P[i], i - l)
-			mx = if (l == i - 1) cur else if (cur > mx) cur else continue
-			if (cur.a >= 0) {
-				++ret[i]
-				++ret[l]
-			}
-		}
+	val AB = Array(2) { readLine()!!.split(" ").map { it.toInt() }.toIntArray() }
+	AB[0].sort()
+	AB[1].sortDescending()
+	var ret = 0
+	repeat(n) {
+		ret += AB[0][it] * AB[1][it]
 	}
-	println(ret.maxOf { it })
+	println(ret)
 }
