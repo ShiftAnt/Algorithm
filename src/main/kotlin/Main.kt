d579ce6262a7841fb2fb1a3a12413162961b6eff
김지웅 <kjw4611@gmail.com>
2022-05-17 18:08:32 +0900

    [BJ 1032] 명령 프롬프트

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,20 @@
-fun ArrayDeque<Int>.toNum(): String {
-	val ret = StringBuilder()
-	for (ele in this) {
-		ret.append(ele)
-	}
-	return ret.toString()
-}
-
 fun main() {
-	var n = readLine()!!.toInt()
-	val num = ArrayDeque<Int>()
-	num += 0
+	val n = readLine()!!.toInt()
 
-	while (n-- != 0) {
-		val siz = num.size
-		var idx = 0
-		for (i in siz - 2 downTo 0) {
-			if (num[i] - num[i + 1] != 1) {
-				idx = i + 1
-				break
+	val P = Array(n) { readLine()!! }
+
+	val ret = StringBuilder()
+
+	loop@
+	for (i in P[0].indices) {
+		val char = P[0][i]
+		for (j in 1 until n) {
+			if (char != P[j][i]) {
+				ret.append("?")
+				continue@loop
 			}
 		}
-		if (idx == 0 && num[idx] == 9) {
-			if (num.size >= 10) {
-				println("-1")
-				return
-			}
-			num.addFirst(num.size)
-		}
-		else ++num[idx]
-		for (i in num.size - 1 downTo idx + 1) {
-			num[i] = num.size - 1 - i
-		}
+		ret.append(char)
 	}
-	println(num.toNum())
+	println(ret)
 }
