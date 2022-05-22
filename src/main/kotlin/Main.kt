e24cf6937846527aae375f5d69da767ce88b1568
김지웅 <kjw4611@gmail.com>
2022-05-22 12:24:22 +0900

    [BJ 1051] 숫자 정사각형

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
-fun main() {
-	val n = readLine()!!.toInt()
-	val P = readLine()!!.split(" ").map { it.toInt() }.sorted()
+import kotlin.math.*
 
-	println(P[0] * P[n - 1])
+fun main() {
+	val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+
+	val P = Array(n) { readLine()!! }
+
+	for (siz in min(n, m) downTo 1) {
+		for (i in 0..n - siz) {
+			for (j in 0..m - siz) {
+				if (P[i][j] == P[i][j + siz - 1] && P[i][j] == P[i + siz - 1][j] && P[i][j] == P[i + siz - 1][j + siz - 1]) {
+					println(siz * siz)
+					return
+				}
+			}
+		}
+	}
 }
