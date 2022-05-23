c079e7c0741fdcf76fdf8d75295fd5fb87634bbf
김지웅 <kjw4611@gmail.com>
2022-05-23 21:44:02 +0900

    [BJ 1063] 킹

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,53 @@
-import kotlin.math.*
+val n = 8
+
+fun sToI(s: String) = intArrayOf(s[1] - '1', s[0] - 'A')
+
+fun iToS(i: IntArray) = "${'A' + i[1]}${'1' + i[0]}"
+
+val dr = arrayOf(0, 0, -1, 1, 1, 1, -1, -1)
+val dc = arrayOf(1, -1, 0, 0, 1, -1, 1, -1)
 
 fun main() {
-	val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+	val (y, x, M) = readLine()!!.split(" ")
 
-	val P = Array(n) { readLine()!! }
+	val m = M.toInt()
 
-	for (siz in min(n, m) downTo 1) {
-		for (i in 0..n - siz) {
-			for (j in 0..m - siz) {
-				if (P[i][j] == P[i][j + siz - 1] && P[i][j] == P[i + siz - 1][j] && P[i][j] == P[i + siz - 1][j + siz - 1]) {
-					println(siz * siz)
-					return
-				}
-			}
+	val P = Array(n) {IntArray(n)}
+
+	val a = sToI(y)
+	val b = sToI(x)
+
+	P[a[0]][a[1]] = -1
+	P[b[0]][b[1]] = 1
+
+	repeat(m) {
+		val dir = when (readLine()) {
+			"R" -> 0
+			"L" -> 1
+			"B" -> 2
+			"T" -> 3
+			"RT" -> 4
+			"LT" -> 5
+			"RB" -> 6
+			"LB" -> 7
+			else -> -1
 		}
+		val ny = a[0] + dr[dir]
+		val nx = a[1] + dc[dir]
+		try {
+			if (ny in 0 until n && nx in 0 until n) {
+				if (P[ny][nx] == 1) {
+					P[b[0] + dr[dir]][b[1] + dc[dir]] = 1
+					b[0] += dr[dir]
+					b[1] += dc[dir]
+				}
+				P[a[0]][a[1]] = 0
+				a[0] = ny
+				a[1] = nx
+				P[ny][nx] = -1
+			}
+		} catch (_: Exception) {}
 	}
+	println(iToS(a))
+	println(iToS(b))
 }
