19b4501d3126f32b59f751da8e0de45f56b4d096
김지웅 <kjw4611@gmail.com>
2022-05-24 20:34:01 +0900

    [BJ 1068] 트리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,53 +1,30 @@
-val n = 8
-
-fun sToI(s: String) = intArrayOf(s[1] - '1', s[0] - 'A')
-
-fun iToS(i: IntArray) = "${'A' + i[1]}${'1' + i[0]}"
-
-val dr = arrayOf(0, 0, -1, 1, 1, 1, -1, -1)
-val dc = arrayOf(1, -1, 0, 0, 1, -1, 1, -1)
-
 fun main() {
-	val (y, x, M) = readLine()!!.split(" ")
+	val n = readLine()!!.toInt()
+	var stt = -1
 
-	val m = M.toInt()
+	val P = readLine()!!.split(" ").map { it.toInt() }
 
-	val P = Array(n) {IntArray(n)}
+	val al = Array(n) { ArrayList<Int>() }
 
-	val a = sToI(y)
-	val b = sToI(x)
-
-	P[a[0]][a[1]] = -1
-	P[b[0]][b[1]] = 1
-
-	repeat(m) {
-		val dir = when (readLine()) {
-			"R" -> 0
-			"L" -> 1
-			"B" -> 2
-			"T" -> 3
-			"RT" -> 4
-			"LT" -> 5
-			"RB" -> 6
-			"LB" -> 7
-			else -> -1
-		}
-		val ny = a[0] + dr[dir]
-		val nx = a[1] + dc[dir]
-		try {
-			if (ny in 0 until n && nx in 0 until n) {
-				if (P[ny][nx] == 1) {
-					P[b[0] + dr[dir]][b[1] + dc[dir]] = 1
-					b[0] += dr[dir]
-					b[1] += dc[dir]
-				}
-				P[a[0]][a[1]] = 0
-				a[0] = ny
-				a[1] = nx
-				P[ny][nx] = -1
-			}
-		} catch (_: Exception) {}
+	repeat(n) {
+		if (P[it] == -1) stt = it
+		else al[P[it]] += it
 	}
-	println(iToS(a))
-	println(iToS(b))
+	val vstd = BooleanArray(n)
+	var ret = 0
+	val except = readLine()!!.toInt()
+	fun dfs(idx: Int): Int {
+		vstd[idx] = true
+		if (idx == except) return 0
+		var child = 1
+
+		for (nxt in al[idx]) {
+			if (vstd[nxt]) continue
+			child += dfs(nxt)
+		}
+		if (child == 1) ++ret
+		return child
+	}
+	dfs(stt)
+	println(ret)
 }
