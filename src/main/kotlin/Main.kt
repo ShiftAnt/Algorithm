24fe12154633257bbd683eeae33b2c270d19b2f5
김지웅 <kjw4611@gmail.com>
2022-05-27 23:56:25 +0900

    [BJ 10718] We love kriii

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,3 @@
 fun main() {
-	val (a, b) = readLine()!!.split(" ")
-	val stt = StringBuilder()
-	repeat(b.length - a.length) {
-		stt.append("0")
-	}
-	stt.append(a)
-	var ret = 0
-	for (i in b.indices) {
-		if (stt[i] == b[i]) {
-			if (b[i] == '8') ++ret
-		}
-		else break
-	}
-	println(ret)
+	println("강한친구 대한육군\n강한친구 대한육군")
 }
