4e067b4269e7bbd646d855bb74cf3c4e82c12322
김지웅 <kjw4611@gmail.com>
2022-05-28 16:24:25 +0900

    [BJ 2631] 줄세우기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+import kotlin.math.*
+
 fun main() {
-	println("강한친구 대한육군\n강한친구 대한육군")
+	val n = readLine()!!.toInt()
+	val P = IntArray(n) { readLine()!!.toInt() }
+	val dp = IntArray(n) {1}
+	for (i in P.size - 1 downTo 0) {
+		for (j in i + 1 until  P.size) {
+			if (P[i] < P[j]) {
+				dp[i] = max(dp[i], dp[j] + 1)
+			}
+		}
+	}
+	println(P.size - dp.maxOf { it })
 }
